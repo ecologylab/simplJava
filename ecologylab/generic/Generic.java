2f26ba37e8c1f0331d6ed68c8ccf563457c33c23
andruid <andruid@ecologylab.net>
2003-03-09 06:15:06 +0000

    added convenient parameter...() methods

--- ecologylab/generic/Generic.java
+++ ecologylab/generic/Generic.java
@@ -15,8 +15,76 @@ import java.util.Date;
  */
 public class Generic 
 {
+/**
+ * Get a boolean parameter from the runtime environment. If the value is the
+ * string <code>true</code> or <code>yes</code>, the result is 
+ * <code>true</code>; else false.
+ * 
+ * @param	name	The name of the parameter's key.
+ */
+   public boolean parameterBool(String name)
+   {
+      String value	= parameter(name);
+      boolean result	= value != null;
+      if (result)
+	 result		=  value.equalsIgnoreCase("true") ||
+	    value.equalsIgnoreCase("yes") || (value.equals("1"));
+      return result;
+   }
+/**
+ * Get an integer parameter from the runtime environment. The default is 0.
+ * 
+ * @param	name	The name of the parameter's key.
+ */
+   public int parameterInt(String paramName)
+   { return parameterInt(paramName, 0); }
+   
+/**
+ * Get an integer parameter from the runtime environment. 
+ * 
+ * @param	name		The name of the parameter's key.
+ * @param	defaultValue	Default integer value, in case param is 
+ *				unspecified in the runtime env.
+ */
+   public int parameterInt(String paramName, int defaultValue)
+   {
+      String paramValue	= parameter(paramName);
+      int result	= defaultValue;
+      if (paramValue != null)
+	 try
+	 {
+	    result	= Integer.parseInt(paramValue);
+	 } catch (NumberFormatException e)
+	 {
+	    Debug.println("bad number format: "+paramName+"="+paramValue);
+	 }
+      return result;
+   }
+/**
+ * Get a float parameter from the runtime environment.
+ * 
+ * @param	name		The name of the parameter's key.
+ * @param	defaultValue	Default floating point value, in case param is 
+ *				unspecified in the runtime env.
+ */
+   public static float parameterFloat(String paramName, float defaultValue)
+   {
+      String paramValue	= parameter(paramName);
+      float result	= defaultValue;
+      if (paramValue != null)
+      {
+	 float parsedValue	= Generic.parseFloat(paramValue);
+	 if (!Float.isNaN(parsedValue))
+	    result	= parsedValue;
+      }
+      return result;
+   }
+   public static final String parameter(String paramName)
+   {
 
-	/**
+      return Environment.the.get().parameter(paramName);
+   }
+/**
  * Turn a string into a float.
  * 
  * @return	the float, if the String is cool; else Float.NaN
@@ -34,6 +102,7 @@ public class Generic
       }
       return result;
    }
+
 
 	/**
  * Sleep easily, ignoring (unlikely) <code>InterruptedException</code>s.
@@ -108,7 +177,7 @@ public class Generic
       Thread.currentThread().setPriority(priority);
    }
 
-	   public static boolean contains(String in, String toMatch)
+   public static boolean contains(String in, String toMatch)
    {
       return (in == null) ? false : in.indexOf(toMatch) != -1;
    }
