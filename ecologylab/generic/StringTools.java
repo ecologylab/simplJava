b14600a2eaeacab337f3b2e7bd8990445e08fa02
andruid <andruid@ecologylab.net>
2003-03-10 23:10:04 +0000

    added pageString(), tightened toLowerCase()

--- ecologylab/generic/StringTools.java
+++ ecologylab/generic/StringTools.java
@@ -1,6 +1,7 @@
 package cm.generic;
 
 import java.lang.*;
+import java.net.*;
 
 /**
  * @author andruid
@@ -8,19 +9,25 @@ import java.lang.*;
  * Methods for manipulating </ode>String</code> and <code>StringBuffer</code>s.
  */
 public class StringTools
+extends Debug
 {
 /**
  * Changes the StringBuffer to lower case, in place, without any new storage
  * allocation.
  */
-   public static void toLowerCase(StringBuffer buffer)
+   public static final void toLowerCase(StringBuffer buffer)
    {
       int length	= buffer.length();
       for (int i=0; i<length; i++)
       {
 	 char c		= buffer.charAt(i);
-	 if (Character.isLowerCase(c))
-	    buffer.setCharAt(i, Character.toLowerCase(c));
+//	 if (Character.isUpperCase(c))
+	 // A = 0x41, Z = 0x5A; a = 0x61, z = 0x7A
+	 if ((c >='A') && (c <= 'Z'))
+	 {
+	    c	       += 0x20;
+	    buffer.setCharAt(i, c);
+	 }
       }
    }
 /**
@@ -30,8 +37,38 @@ public class StringTools
  * In those cases, <i>much</i> better than 
  * <code>new String(StringBuffer)</code>
  */
-   public static String toString(StringBuffer buffer)
+   public static final String toString(StringBuffer buffer)
    {
       return buffer.substring(0);
+   }
+   public static final boolean contains(String in, String toMatch)
+   {
+      return (in == null) ? false : in.indexOf(toMatch) != -1;
+   }
+
+/**
+ * Very efficiently forms String representation of url (better than 
+ * <code>URL.toExternalForm()</code>). Doesn't include query or anchor.
+ */
+   public static final String pageString(URL u)
+   {
+      String protocol	= u.getProtocol();
+      String authority	= u.getAuthority(); // authority is host:port
+      String path	= u.getPath();	    // doesn't include query
+
+      // pre-compute length of StringBuffer
+      int length	=
+	 protocol.length() + 3 /* :// */ + authority.length() + path.length();
+
+      StringBuffer result = new StringBuffer(length);
+      result.append(protocol).append("://").append(authority).append(path);
+
+      return new String(result);
+   }
+   public static void main(String[] args)
+   {
+      for (int i=0; i<args.length; i++)
+	 println(pageString(Generic.getURL(args[i], "oops " + i)));
+	 
    }
 }
