7282b4f221659644e1769e7ad2a8f077d4d6a8df
andruid <andruid@ecologylab.net>
2003-03-11 17:35:02 +0000

    tuned text colorings.

--- ecologylab/generic/Colors.java
+++ ecologylab/generic/Colors.java
@@ -18,18 +18,20 @@ public interface Colors
    public static final int	R		= 0xff0000;
    public static final int	G		= 0xff00;
    public static final int	B		= 0xff;
-   
+
    public static final float	YELLOW	= 60.0f/360.0f;
    public static final float	RED	= 0/360.0f;
    public static final float	GREEN	= 120.0f/360.0f;
    public static final float	ORANGE	= 30.0f/360.0f;
-   public static final float	MAGENTA	= 300.0f/360.0f;
-   public static final float	BLUE	= 240.0f/360.0f;
+   public static final float	MAGENTA	= 312.0f/360.0f;
+//   public static final float	MAGENTA	= 300.0f/360.0f;
+   public static final float	PURPLE	= 280.0f/360.0f;
+   public static final float	BLUE	= 248.0f/360.0f;
    public static final float	YELLOW_GREEN	= 90.0f/360.0f;
    public static final float	RED_ORANGE	= 15.0f/360.0f;
    public static final float	BLUE_MAGENTA	= 270.0f/360.0f;
    public static final float	CYAN	= 200.0f/360.0f;
    public static final float	YELLOW_ORANGE	= 45.0f/360.0f;
    public static final float	RED_MAGENTA	= 150.0f/360.0f;
-
+   
 }
