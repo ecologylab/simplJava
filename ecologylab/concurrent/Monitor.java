98ce6948476df557d9aa895c8fe9b83ca5fa3db4
andruid <andruid@ecologylab.net>
2003-08-20 22:33:12 +0000

    New base class with generic methods for wait(Object o), and notifyAll().

---
+++ ecologylab/concurrent/Monitor.java
@@ -0,0 +1,28 @@
+package cm.generic;
+
+public class Monitor
+extends ObservableDebug
+{
+   public static void wait(Object toLock)
+   {
+      synchronized (toLock)
+      {
+	 try
+	 {
+	    toLock.wait();
+//	    debug("dispatchDownloads() notified");
+	 } catch (InterruptedException e)
+	 {
+	    // interrupt means stop
+	 }
+      }
+   }
+
+   public static void notifyAll(Object lock)
+   {
+      synchronized (lock)
+      {
+	 lock.notifyAll();
+      }
+   }
+}
