eff67fb94437d871d18bede1e0460dadbd229936
andruid <andruid@ecologylab.net>
2004-06-19 09:07:28 +0000

    Make dowloandThread creation subclassable, and added NewPorterStemmer creation in TextDownloadMonitor.

---
+++ ecologylab/concurrent/TextDownloadMonitor.java
@@ -0,0 +1,50 @@
+package cm.generic;
+
+import cm.media.text.NewPorterStemmer;
+
+import java.util.*;
+
+/**
+ * A DownloadMonitor that processes text.
+ * It makes one NewPorterStemmer's available to each thread.
+ */
+public class TextDownloadMonitor
+extends
+DownloadMonitor
+{
+   static HashMap	stemmersHash	= new HashMap();
+   
+   public TextDownloadMonitor(String name, int numDownloadThreads, 
+			  boolean getUrgent)
+   {
+      super(name, numDownloadThreads, getUrgent);
+   }
+
+   protected Thread makeDownloadThread(int i, String s)
+   {
+      Thread result = super.makeDownloadThread(i, s);
+      debugA("creating stemmer for " + result);
+      
+      addStemmer(result);
+
+      return result;
+   }
+   public static void addStemmer()
+   {
+      addStemmer(Thread.currentThread());
+   }
+   static void addStemmer(Thread thread)
+   {
+      NewPorterStemmer stemmer			= new NewPorterStemmer();
+      stemmersHash.put(thread, stemmer);
+   }
+
+   public static NewPorterStemmer getStemmer()
+   {
+      Thread currentThread		= Thread.currentThread();
+      NewPorterStemmer stemmer		= 
+	 (NewPorterStemmer) stemmersHash.get(currentThread);
+      return stemmer;
+   }
+}
+
