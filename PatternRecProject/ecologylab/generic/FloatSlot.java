d766065ce2df01ca3eebb58bce33f94a6700781d
noathor <noauthor@ecologylab.net>
2004-11-07 22:43:41 +0000

    This commit was manufactured by cvs2svn to create branch 'PatternRecProject'.

---
+++ PatternRecProject/ecologylab/generic/FloatSlot.java
@@ -0,0 +1,14 @@
+package cm.generic;
+
+import java.lang.Object;
+
+public class FloatSlot
+extends Object
+{
+   public float 	value;
+   public FloatSlot(float b)
+     {
+	super();
+	value = b;
+     }
+}
