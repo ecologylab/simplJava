a56dccc32ac24a36533fc719225aecd106d21ef8
noathor <noauthor@ecologylab.net>
2004-11-07 22:43:46 +0000

    This commit was manufactured by cvs2svn to create branch 'working_pattern_cm'.

---
+++ working_pattern_cm/ecologylab/generic/ThreadMaster.java
@@ -0,0 +1,21 @@
+package cm.generic;
+
+/**
+ * Implement this class in an object that knows about many other threads.
+ * Then, use the provided methods to have that object pause many
+ * threads at once. This is done in order to let the CPU concentrate
+ * on a specific intensive activity, like handling interactive drag and drop,
+ * or pruning large collections.
+ */
+public interface
+ThreadMaster
+{
+/**
+ * Pause all the threads we know about.
+ */
+   public void pauseThreads();
+/**
+ * Unpause (continue) all the threads we know about.
+ */
+   public void unpauseThreads();
+}
