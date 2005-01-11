d8533c1f00e18812eb209541e3dfc117fce12d84
noathor <noauthor@ecologylab.net>
2005-01-11 23:28:27 +0000

    This commit was manufactured by cvs2svn to create branch 'cm_metadata'.

---
+++ cm_metadata/ecologylab/xml/library/rss/RssState.java
@@ -0,0 +1,15 @@
+package ecologylab.xml.rss;
+
+import ecologylab.xml.*;
+
+public class RssState extends ElementState
+{
+   public Channel		channel;
+
+   public static void initTranslations()
+   {
+	  addTranslation("ecologylab.xml.rss", "RssState");
+	  addTranslation("ecologylab.xml.rss", "Item");
+	  addTranslation("ecologylab.xml.rss", "Channel");
+   }
+}
