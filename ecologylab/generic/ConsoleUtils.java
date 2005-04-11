9845c228175f9b4dd377b9d5562a4c105464d04c
blake <blake@ecologylab.net>
2005-04-11 04:20:11 +0000

    I fixed the bug that caused certain delete operations that split TextTokens to cause editing to reach an unsafe state. I also made the delete key work as expected.
    I also updated our system directory structure to work properly with linux and mac os x.

---
+++ ecologylab/generic/ConsoleUtils.java
@@ -0,0 +1,18 @@
+package cm.generic;
+
+/**
+ * Little group of utilities for making console debugging a little easier.
+ * 
+ * @author blake
+ */
+public class ConsoleUtils extends Debug
+{
+	public static void obtrusiveConsoleOutput(String outputMessage)
+	{
+		System.out.println("****************************************************");
+		System.out.println("----------------------------------------------------");
+		System.out.println(outputMessage);
+		System.out.println("----------------------------------------------------");
+		System.out.println("****************************************************");
+	}
+}
