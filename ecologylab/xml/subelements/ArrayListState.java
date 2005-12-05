a86dc678c98f64da9698ccdbdf1e18879f8002ed
andruid <andruid@ecologylab.net>
2005-12-05 19:16:54 +0000

    *** empty log message ***

---
+++ ecologylab/xml/subelements/ArrayListState.java
@@ -0,0 +1,35 @@
+package ecologylab.xml;
+
+import java.util.ArrayList;
+
+/**
+ * An ElementState XML tree node that supports an ArrayList of
+ * children (as well as whatever else you add to it).
+ * 
+ * @author andruid
+ */
+public class ArrayListState extends ElementState
+{
+	public ArrayList set	=	new ArrayList();
+		
+	public ArrayListState()
+	{
+		super();
+	}
+
+   /**
+    * Add an element to our Collection.
+    */
+   	public void addNestedElement(ElementState elementState)
+   	{
+   		set.add(elementState);
+   	}
+   	/**
+   	 * Remove all elements from our Collection.
+   	 *
+   	 */
+   	public void clear()
+   	{
+   		set.clear();
+   	}
+}
