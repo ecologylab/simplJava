208a3a9247ed39e4e7e2ad815f3644d8f3ac58e3
andruid <andruid@ecologylab.net>
2006-02-11 06:37:23 +0000

    Use new ElementState api

--- ecologylab/xml/rss/Item.java
+++ ecologylab/xml/rss/Item.java
@@ -1,11 +1,31 @@
 package ecologylab.xml.rss;
 
+import java.util.HashMap;
+
+import ecologylab.generic.ParsedURL;
 import ecologylab.xml.*;
 
 public class Item extends ElementState
 {
-   public ElementState			title;
-   public ElementState			link;
-   public ElementState			description;
+   public String				title;
+  // public ElementState			title;
+   //public ElementState			link;
+   public String				description;
+   public ParsedURL				link;
+   //public String				description;
    public ElementState			author;
+   
+   static HashMap				elementsWithOneTextChild	= new HashMap();
+   
+   static
+   {
+	   elementsWithOneTextChild.put("title", "title");
+	   elementsWithOneTextChild.put("link", "link");
+	   elementsWithOneTextChild.put("description", "description");
+   }
+   
+   public HashMap fieldsAsElementWithOneTextChild()
+   {
+	   return elementsWithOneTextChild;
+   }
 }
