cf88b6d140988586b745c0b61099bca244a49dea
andruid <andruid@ecologylab.net>
2006-02-27 07:14:01 +0000

    Use latest API for declaring leaf nodes.

--- ecologylab/xml/library/rss/Item.java
+++ ecologylab/xml/library/rss/Item.java
@@ -5,27 +5,26 @@ import java.util.HashMap;
 import ecologylab.generic.ParsedURL;
 import ecologylab.xml.*;
 
-public class Item extends ElementState
+/**
+ * RSS 1 item element declaration.
+ *
+ * @author andruid
+ */
+public class Item extends ElementStateWithLeafElements
 {
    public String				title;
   // public ElementState			title;
    //public ElementState			link;
    public String				description;
    public ParsedURL				link;
-   //public String				description;
-   public ElementState			author;
+   //public ElementState		description;
+   public String				author;
    
-   static HashMap				elementsWithOneTextChild	= new HashMap();
+   static final String[]		LEAF_ELEMENT_FIELD_NAMES	= {"title", "link", "description", "author"};
    
    static
    {
-	   elementsWithOneTextChild.put("title", "title");
-	   elementsWithOneTextChild.put("link", "link");
-	   elementsWithOneTextChild.put("description", "description");
+	   defineLeafElementFieldNames(LEAF_ELEMENT_FIELD_NAMES);
    }
    
-   public HashMap fieldsAsElementWithOneTextChild()
-   {
-	   return elementsWithOneTextChild;
-   }
 }
