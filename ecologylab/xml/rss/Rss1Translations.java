e669e60b413bb1fee5beedfcdddd06bad38591a0
andruid <andruid@ecologylab.net>
2006-02-27 07:13:51 +0000

    Define a space of translations for version 1.0 style RSS.

---
+++ ecologylab/xml/rss/Rss1Translations.java
@@ -0,0 +1,17 @@
+package ecologylab.xml.rss;
+
+import ecologylab.xml.NameSpace;
+
+/**
+ * Translations for RSS version 1.0 style, with no namespace: style identifiers.
+ *
+ * @author andruid
+ */
+public class Rss1Translations
+extends NameSpace
+{
+   public Rss1Translations()
+   {
+	   super("rss", "ecologylab.xml.rss");
+   }
+}
