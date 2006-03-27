755c42ad769fbf2921954c2195462c8a3b1b5d58
andruid <andruid@ecologylab.net>
2006-03-27 17:00:08 +0000

    Add done() stuff.

--- ecologylab/generic/Monitor.java
+++ ecologylab/generic/Monitor.java
@@ -6,19 +6,53 @@ package ecologylab.generic;
 public class Monitor
 extends ObservableDebug
 {
-   public static void wait(Object toLock)
+   boolean done;
+   
+   /**
+    * Create a Monitor with a single done flag, that can be waited on.
+    *
+    */
+   public Monitor()
+   {
+	   
+   }
+   /**
+    * If this is not yet done, then wait(); 
+    * else return immediately.
+    */
+   public synchronized void waitForDone()
+   {
+	   if (!done)
+		   wait(this);
+   }
+   /**
+    * Mark this as done, as needed, and notify all waiting threads.
+    */
+   public synchronized void done()
+   {
+	   done		= true;
+	   notifyAll(this);
+   }
+   /**
+    * Short-hand form: wait for the Object. Handles Interrupted exceptions.
+    * @param toLock
+    * @return	true if the wait was completed without getting interrupted.
+    */
+   public static boolean wait(Object toLock)
    {
       synchronized (toLock)
       {
-	 try
-	 {
-	    toLock.wait();
-//	    debug("dispatchDownloads() notified");
-	 } catch (InterruptedException e)
-	 {
-	    // interrupt means stop
-	    e.printStackTrace();
-	 }
+		 try
+		 {
+		    toLock.wait();
+		    return true;
+	//	    debug("dispatchDownloads() notified");
+		 } catch (InterruptedException e)
+		 {
+		    // interrupt means stop
+		    e.printStackTrace();
+		    return false;
+		 }
       }
    }
 
@@ -26,14 +60,14 @@ extends ObservableDebug
    {
       synchronized (lock)
       {
-	 lock.notifyAll();
+		 lock.notifyAll();
       }
    }
    public static void notify(Object lock)
    {
       synchronized (lock)
       {
-	 lock.notify();
+		 lock.notify();
       }
    }
 }
