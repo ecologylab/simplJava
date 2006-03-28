80e408c99431188256d97da578edd092c4db0de1
andruid <andruid@ecologylab.net>
2006-03-28 01:54:03 +0000

    Improve parsing by using new style leaf node api.

--- ecologylab/xml/rss/Channel.java
+++ ecologylab/xml/rss/Channel.java
@@ -3,14 +3,21 @@ package ecologylab.xml.rss;
 import ecologylab.xml.*;
 import java.util.ArrayList;
 
-public class Channel extends ElementState
+public class Channel extends ElementStateWithLeafElements
 {
-   public ElementState			title;
-   public ElementState			description;
+   public String			title;
+   public String			description;
    
    public ArrayList itemSet	=	new ArrayList();
 	
-   	public void addNestedElement(ElementState elementState)
+   static final String[]		LEAF_ELEMENT_FIELD_NAMES	= {"title", "description"};
+   
+   static
+   {
+	   defineLeafElementFieldNames(LEAF_ELEMENT_FIELD_NAMES);
+   }
+   
+   public void addNestedElement(ElementState elementState)
 	   throws XmlTranslationException
    	{
    		if (elementState instanceof Item)
