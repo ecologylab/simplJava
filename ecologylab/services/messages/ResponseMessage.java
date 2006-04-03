016c3eeeea12851f269a89b37929a7f936522d9d
andruid <andruid@ecologylab.net>
2006-04-03 19:03:54 +0000

    Add abstract isOK() message. Get rid of String

--- ecologylab/services/messages/ResponseMessage.java
+++ ecologylab/services/messages/ResponseMessage.java
@@ -2,47 +2,28 @@ package ecologylab.services.messages;
 
 /**
  * Service response message.
+ * 
  * @author blake
- *
+ * @author andruid
  */
-public class ResponseMessage 
+abstract public class ResponseMessage 
 extends ServiceMessage
-implements ResponseTypes
 {
-	public String response;
-	
-	private static ResponseMessage OKResponse 	= null;
-	
 	public ResponseMessage() {}
 	
-	public ResponseMessage(String response)
-	{
-		this.response = response;
-	}
-	
-	/**
-	 * Returns a ResponseMessage object representing the ReponseTypes 'OK'
-	 * 
-	 * @return A ReponseMessage representing 'OK'
-	 */
-	public static ResponseMessage OKResponse()
-	{
-		if (OKResponse == null)
-			OKResponse = new ResponseMessage(OK);
-		return OKResponse;
-	}
+    /**
+     * Let's the client easily test for OK or error.
+     * 
+     * @return
+     */
+    public abstract boolean isOK();
 
     /**
-     * @return Returns the response.
+     * Allows for custom processing of ResponseMessages by ServicesClient, without extending that.
+     *
      */
-    public String getResponse() {
-        return response;
-    }
-
-    /**
-     * @param response The response to set.
-     */
-    public void setResponse(String response) {
-        this.response = response;
+    public void processResponse()
+    {
+    	
     }
 }
