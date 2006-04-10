7de12665b98d85733235267f720e5e6bd0481428
toupsz <toupsz@ecologylab.net>
2006-04-10 14:41:58 +0000

    Removed unnecessary imports.

--- ecologylab/services/ServerToClientConnection.java
+++ ecologylab/services/ServerToClientConnection.java
@@ -1,9 +1,7 @@
 package ecologylab.services;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 
