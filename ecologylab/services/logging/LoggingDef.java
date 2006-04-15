71c31bddf3f74df2e7d7d1fc333a1dba6d5738eb
eunyee <eunyee@ecologylab.net>
2006-04-15 21:03:51 +0000

    delete maxSize

--- ecologylab/services/logging/LoggingDef.java
+++ ecologylab/services/logging/LoggingDef.java
@@ -14,14 +14,10 @@ public interface LoggingDef
 	/**
 	 * We use loggin server hosted on csdll.cs.tamu.edu
 	 */
-//	static final String loggingServer = "128.194.147.58";
+//	static final String loggingServer = "128.194.147.58";  // CSDLL server IP address
 	static final String loggingServer = Logging.localHost();
+//	static final String loggingServer = "128.194.147.49";  // ecology1
 	static final int port = 10000;
-	
-	/**
-	 * we set the maximum size of logging message as a 16kbyte
-	 */
-	static final int maxSize = 60000; 
 	
 	/**
 	 * Logging file of the server's debug messages
