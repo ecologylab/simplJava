2f2202d66e7bee3fc92a0dd4a168c9e4d440e04d
toupsz <toupsz@ecologylab.net>
2006-04-17 12:30:11 +0000

    Added more JavaDoc comments.

--- ecologylab/services/authentication/AuthenticationMessages.java
+++ ecologylab/services/authentication/AuthenticationMessages.java
@@ -1,15 +1,37 @@
 package ecologylab.services.authentication;
 
-public interface AuthenticationMessages 
+/**
+ * Interface that contains the list of constant Strings used for Authentication
+ * messages. Implement this interface if your class uses Login or Logout.
+ * 
+ * @author Zach Toups (toupsz@gmail.com)
+ */
+public interface AuthenticationMessages
 {
-	public static String OK 	= "ok";
-	public static String BADTransmission 	= "bad";
-    
-    public static String LOGIN_SUCCESSFUL = "Successfully logged in.";
-    
-    public static String LOGIN_FAILED_PASSWORD = "Cannot log in: username/password combination not found.";
-    public static String LOGIN_FAILED_LOGGEDIN = "Cannot log in: username already logged-in.";
-    public static String LOGOUT_FAILED_NOT_LOGGEDIN = "Cannot log out: username was not logged-in.";
+    /**
+     * Indicates that a LOGIN was successful.
+     */
+    public static final String LOGIN_SUCCESSFUL = "Successfully logged in.";
 
-    public static String REQUEST_FAILED_NOT_AUTHENTICATED = "Cannot process request, connection not yet authenticated.";
+    /**
+     * Indicates that login failed because either the username was not found, or
+     * because the password did not match a username.
+     */
+    public static final String LOGIN_FAILED_PASSWORD = "Cannot log in: username/password combination not found.";
+
+    /**
+     * Indicates that login failed because the username is already logged in.
+     */
+    public static final String LOGIN_FAILED_LOGGEDIN = "Cannot log in: username already logged-in.";
+
+    /**
+     * Indicates that LOGOUT failed because the given username was never logged
+     * in in the first place!
+     */
+    public static final String LOGOUT_FAILED_NOT_LOGGEDIN = "Cannot log out: username was not logged-in.";
+
+    /**
+     * Indicates that a request failed because no one has logged in yet.
+     */
+    public static final String REQUEST_FAILED_NOT_AUTHENTICATED = "Cannot process request, connection not yet authenticated.";
 }
