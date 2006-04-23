c420076e7f3a36d25ee7284dc629cef5118c633d
andruid <andruid@ecologylab.net>
2006-04-23 16:49:48 +0000

    ServicesHostsAndPorts used to be called ServicesServerAddressesAndPorts. Added CF_SERVICES_SERVER.

---
+++ ecologylab/services/distributed/common/ServicesHostsAndPorts.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Apr 5, 2006
+ */
+package ecologylab.services;
+
+/**
+ * A place to gather host and port assignments in the Interface Ecology Lab.
+ *
+ * @author andruid
+ */
+public interface ServicesHostsAndPorts
+{
+    public static final int 	ROGUE_PORT 			= 14444;
+    
+	public static final int		CF_SERVICES_PORT	= 8082;
+    
+}
