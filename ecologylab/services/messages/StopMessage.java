c0dfdd5948b9bed4b88b9e37a4c22e9348e9a892
andruid <andruid@ecologylab.net>
2006-04-28 23:23:20 +0000

    Call stop() on main MAIN_START_AND_STOPPABLE. Disconnect the client if a response is received.

--- ecologylab/services/messages/StopMessage.java
+++ ecologylab/services/messages/StopMessage.java
@@ -1,13 +1,18 @@
 package ecologylab.services.messages;
 
 import ecologylab.generic.ObjectRegistry;
+import ecologylab.generic.StartAndStoppable;
+import ecologylab.services.ServicesClient;
+import ecologylab.services.SessionObjects;
 
 /**
  * Informs the service server to shut down.
  * 
  * @author blake
  */
-public class StopMessage extends RequestMessage 
+public class StopMessage
+extends RequestMessage
+implements SessionObjects
 {
 
 	/**
@@ -16,6 +21,24 @@ public class StopMessage extends RequestMessage
 	 */
 	public ResponseMessage performService(ObjectRegistry objectRegistry) 
 	{
-		return null;
+		StartAndStoppable sas = (StartAndStoppable) objectRegistry.lookupObject(MAIN_START_AND_STOPPABLE);
+		debug("performService(): call stop(" + sas);
+		if (sas != null)
+			sas.stop();
+		return new OkResponse(); //shouldn't get here
 	}
+    /**
+     * Allows for custom processing of ResponseMessages by ServicesClient, without extending that.
+     * Stop the server, once we confirm processing of our Stop message.
+     * 
+     * @param objectRegistry provide a context for response message processing.
+     */
+    public void processResponse(ObjectRegistry objectRegistry)
+    {
+		 ServicesClient browserServicesClient	= (ServicesClient) objectRegistry.lookupObject(BROWSER_SERVICES_CLIENT);
+		 if (browserServicesClient != null)
+		 {
+			 browserServicesClient.disconnect();
+		 }
+    }
 }
