8138c0cb48fdfaf6f5b84d6b1c6c659b43148244
toupsz <toupsz@ecologylab.net>
2006-05-29 17:55:22 +0000

    provide authentication functionality hook methods for 2-threaded server.

---
+++ ecologylab/services/authentication/nio/AuthMessageProcessor2Threads.java
@@ -0,0 +1,23 @@
+package ecologylab.services.authentication.nio;
+
+import java.nio.channels.SelectionKey;
+
+import ecologylab.generic.ObjectRegistry;
+import ecologylab.services.nio.ContextManager;
+import ecologylab.services.nio.MessageProcessor2Threads;
+import ecologylab.xml.NameSpace;
+
+public class AuthMessageProcessor2Threads extends MessageProcessor2Threads
+{
+
+    public AuthMessageProcessor2Threads(NameSpace translationSpace,
+            ObjectRegistry registry)
+    {
+        super(translationSpace, registry);
+    }
+
+    protected ContextManager generateClientContext(Object token, SelectionKey key, NameSpace translationSpace, ObjectRegistry registry)
+    {
+        return new AuthContextManager(token, key, translationSpace, registry);
+    }
+}
