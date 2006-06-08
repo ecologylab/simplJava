6fc37371be83c3b3f4e32c94827a6189a97437b7
toupsz <toupsz@ecologylab.net>
2006-06-08 12:02:04 +0000

    Added check for negative i values for get(int).

--- ecologylab/xml/ArrayListState.java
+++ ecologylab/xml/ArrayListState.java
@@ -4,86 +4,102 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- * An ElementState XML tree node that supports an ArrayList of
- * children (as well as whatever else you add to it).
+ * An ElementState XML tree node that supports an ArrayList of children (as well as whatever else
+ * you add to it).
  * 
  * @author andruid
  */
 public class ArrayListState extends ElementState implements Cloneable, Iterable
 {
-	public ArrayList set	=	new ArrayList();
-		
-	public ArrayListState()
-	{
-		super();
-	}
+    public ArrayList set = new ArrayList();
 
-	public void add(ElementState elementState)
-	{
-		try {
-			addNestedElement(elementState);
-		}catch (XmlTranslationException e) {
-			debug("ERROR: " + e.getMessage() + " while translating");
-		}		
-	}
-    
+    public ArrayListState()
+    {
+        super();
+    }
+
+    public void add(ElementState elementState)
+    {
+        try
+        {
+            addNestedElement(elementState);
+        }
+        catch (XmlTranslationException e)
+        {
+            debug("ERROR: " + e.getMessage() + " while translating");
+        }
+    }
+
     public ElementState remove(int i)
     {
         return (ElementState) set.remove(i);
     }
-    
+
     public Iterator iterator()
     {
         return set.iterator();
     }
-    
+
     public void add(int i, ElementState obj)
     {
         set.add(i, obj);
     }
-	
+
+    /**
+     * @param i
+     *            the index of the element to get.
+     * @return the element located at i; if i is greater than the size of set or less than 0,
+     *         returns null.
+     */
     public ElementState get(int i)
     {
-        if (i >= set.size())
+        if (i < 0)
         {
             return null;
-        } else
+        }
+        else if (i >= set.size())
+        {
+            return null;
+        }
+        else
         {
             return (ElementState) set.get(i);
         }
     }
-   /**
-    * Add an element to our Collection.
-    */
-   	public void addNestedElement(ElementState elementState)
-    throws XmlTranslationException
+
+    /**
+     * Add an element to our Collection.
+     */
+    public void addNestedElement(ElementState elementState) throws XmlTranslationException
     {
-   		set.add(elementState);
-   	}
-   	/**
-   	 * Remove all elements from our Collection.
-   	 *
-   	 */
-   	public void clear()
-   	{
-   		set.clear();
-   	}
-   	
-   	/**
-   	 * Get the number of elements in the set.
-   	 * @return
-   	 */
-   	public int size()
-   	{
-   		return set.size();
-   	}
-    
+        set.add(elementState);
+    }
+
+    /**
+     * Remove all elements from our Collection.
+     * 
+     */
+    public void clear()
+    {
+        set.clear();
+    }
+
+    /**
+     * Get the number of elements in the set.
+     * 
+     * @return
+     */
+    public int size()
+    {
+        return set.size();
+    }
+
     public Object clone()
     {
         ArrayListState clone = new ArrayListState();
-        
+
         clone.set = (ArrayList) this.set.clone();
-        
+
         return clone;
     }
 }
