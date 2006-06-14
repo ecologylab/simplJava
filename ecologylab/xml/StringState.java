a44d09bf0931c8655b54188a83836adc8691f2ca
toupsz <toupsz@ecologylab.net>
2006-06-14 21:18:44 +0000

    used to have a String that extends ElementState for use in ArrayListState.

---
+++ ecologylab/xml/StringState.java
@@ -0,0 +1,16 @@
+package ecologylab.xml;
+
+public class StringState extends ElementState
+{
+    public String string;
+    
+    public StringState()
+    {
+        super();
+    }
+    
+    public StringState(String string)
+    {
+        this.string = string;
+    }
+}
