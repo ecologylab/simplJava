451f5f73e7a433e1303b0a95beecf851c4dbaa2c
andruid <andruid@ecologylab.net>
2006-07-10 01:46:03 +0000

    Add attribute fields.

--- ecologylab/xml/library/yahoo/ResultSet.java
+++ ecologylab/xml/library/yahoo/ResultSet.java
@@ -2,6 +2,7 @@ package ecologylab.xml.yahoo;
 
 import ecologylab.xml.ArrayListState;
 import ecologylab.xml.ElementState;
+import ecologylab.xml.TranslationSpace;
 
 /**
  * Top level result set collection element for Yahoo search.
@@ -14,5 +15,15 @@ public class ResultSet extends ArrayListState
 	public int			totalResultsAvailable;
 	public int			totalResultsReturned;
 	
+	public int			firstResultPosition;
 	
+	public String		xmlns;
+	public String		schemaLocation;
+	public String		xsi;
+	
+	
+	public static TranslationSpace translationSpace()
+	{
+		return TranslationSpace.get("yahoo", "ecologylab.xml.yahoo");
+	}
 }
