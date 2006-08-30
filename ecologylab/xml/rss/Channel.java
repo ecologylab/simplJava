912ebdd3ae385ecde71ebfadd89ed62e9f03e8fb
andruid <andruid@ecologylab.net>
2006-08-30 08:05:26 +0000

    Use ArrayListState to make more compact.

--- ecologylab/xml/rss/Channel.java
+++ ecologylab/xml/rss/Channel.java
@@ -4,28 +4,20 @@ import ecologylab.xml.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Channel extends ElementState
+public class Channel extends ArrayListState
 {
    public String			title;
    public String			description;
    
-   public ArrayList itemSet	=	new ArrayList();
-	
-   static final String[]		LEAF_ELEMENT_FIELD_NAMES	= {"title", "description"};
+   static final String[]	LEAF_ELEMENT_FIELD_NAMES	= {"title", "description"};
    
-	/**
-	 * The array of Strings with the names of the leaf elements.
-	 * 
-	 * @return
-	 */
-	protected String[] leafElementFieldNames()
-	{
-		return LEAF_ELEMENT_FIELD_NAMES;
-	}
-
-	protected Collection getCollection(Class thatClass, String tag)
-	{
-  		return Item.class.equals(thatClass) ?
-		   itemSet : null;
-  	}  
+   /**
+    * The array of Strings with the names of the leaf elements.
+    * 
+    * @return
+    */
+   protected String[] leafElementFieldNames()
+   {
+	   return LEAF_ELEMENT_FIELD_NAMES;
+   }
 }
