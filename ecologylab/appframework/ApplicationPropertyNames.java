6538978a6b8e1dbc6443f76d78a55985fc34221f
awebb <awebb@ecologylab.net>
2006-11-03 06:36:05 +0000

    added property names for preferences passed to applets

--- ecologylab/appframework/ApplicationPropertyNames.java
+++ ecologylab/appframework/ApplicationPropertyNames.java
@@ -7,5 +7,9 @@ public interface ApplicationPropertyNames
 	public static final String	FRAME 					= "frame";
 
 	public static final String	GO_PREFIX				= "go_prefix";
+	
+	public static final String	PREFERENCES_SET			= "preferences_set";
+	
+	public static final String	PREFERENCES_SET_ASSET	= "preferences_set_asset";
 
 }
