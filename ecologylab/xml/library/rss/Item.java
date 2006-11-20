f61a18d11c73b4c5b7ef027099a4508d7d22f0b7
andruid <andruid@ecologylab.net>
2006-11-20 06:55:28 +0000

    Incorporate feedburner.

--- ecologylab/xml/library/rss/Item.java
+++ ecologylab/xml/library/rss/Item.java
@@ -4,6 +4,7 @@ import ecologylab.net.ParsedURL;
 import ecologylab.xml.ElementState;
 import ecologylab.xml.dc.Dc;
 import ecologylab.xml.media.Media;
+import ecologylab.xml.feedburner.Feedburner;
 
 /**
  * RSS 1 item element declaration.
@@ -23,6 +24,8 @@ public class Item extends ElementState
    @xml_nested	Dc				dc;
    
    @xml_nested	Media			media;
+   
+   @xml_nested	Feedburner		feedburner;
 
    /**
     * @return Returns the author.
@@ -118,5 +121,27 @@ public class Item extends ElementState
    public void setTitle(String title)
    {
 	   this.title = title;
-   }  
+   }
+   
+   /**
+    * If there is an embedded object handling the feedburner namespace, get the origLink
+    * leaf_node from it.
+    * 
+    * @return
+    */
+   public ParsedURL getFeedburnerOrigLink()
+   {
+	   return (feedburner == null) ? null : feedburner.getOrigLink();
+   }
+   
+   /**
+    * Get what looks like the most direct form of the link URL.
+    *  
+    * @return
+    */
+   public ParsedURL getDirectLink()
+   {
+	   ParsedURL result	= getFeedburnerOrigLink();
+	   return (result != null) ? result : link;
+   }
 }
