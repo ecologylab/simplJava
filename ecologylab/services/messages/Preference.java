81e06ce904778d43998bb91bc8639de28c4c311a
andruid <andruid@ecologylab.net>
2006-12-20 03:04:02 +0000

    Add preferencesRegistry(), register(), lookup(), and lookupString().

--- ecologylab/services/messages/Preference.java
+++ ecologylab/services/messages/Preference.java
@@ -1,5 +1,6 @@
 package ecologylab.services.messages;
 
+import ecologylab.generic.Environment;
 import ecologylab.generic.ObjectRegistry;
 import ecologylab.xml.ArrayListState;
 import ecologylab.xml.ElementState;
@@ -40,26 +41,63 @@ public @xml_inherit class Preference extends ArrayListState
 		return get(0);
 	}
 
+	/**
+	 * Access the name of this Preference.
+	 * 
+	 * @return	The Preference's name.
+	 */
 	public String getName() 
 	{
 		return name;
 	}
 
-	public String getValue() 
+	/**
+	 * Access the value of this Preference.
+	 * 
+	 * @return	The Preference's value.
+	 */
+	public String getValue()
 	{
 		return value;
 	}
 
+	/**
+	 * Set the name of this Preference.
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) 
 	{
 		this.name = name;
 	}
 
+	/**
+	 * Set the value of this Preference.
+	 * 
+	 * @param value
+	 */
 	public void setValue(String value) 
 	{
 		this.value = value;
 	}
-	
+	/**
+	 * The default ObjectRegistry that preferences are stored in for the current application's Environment.
+	 * 
+	 * @return
+	 */
+	protected static ObjectRegistry preferencesRegistry()
+	{
+		return Environment.the.preferencesRegistry();
+	}
+	/**
+	 * Register this into the Environment's default preferencesRegistry.
+	 * 
+	 * @param preferencesRegistry
+	 */
+	public void register()
+	{
+		register(preferencesRegistry());
+	}
 	/**
 	 * Register this into a PreferencesRegistry.
 	 * 
@@ -80,7 +118,41 @@ public @xml_inherit class Preference extends ArrayListState
 			if (value != null)
 				preferencesRegistry.registerObject(this.name, value);
 		}
-		
 	}
 	
+	/**
+	 * Get a full Preference object entry from the registry.
+	 * 
+	 * @param name	The name of the preference.
+	 * 
+	 * @return		The corresponding Preference object.
+	 */
+	public static Preference lookup(String name)
+	{
+		return (Preference) preferencesRegistry().lookupObject(name);
+	}
+	/**
+	 * Get an entry from the registry, with value type String.
+	 * 
+	 * @param name			Key to use to lookup the Preference.
+	 * @param defaultValue	Default value to return if the Preference is not found in the registry.
+	 * 
+	 * @return			String value.
+	 */
+	public static String lookupString(String name, String defaultValue)
+	{
+		Preference pref	= lookup(name);
+		return (pref != null) ? pref.value : defaultValue;
+	}
+	/**
+	 * Get an entry from the registry, with value type String.
+	 * 
+	 * @param name		Key to use to lookup the Preference.
+	 * 
+	 * @return			String value, or null if the preferences registry contains no value for this key..
+	 */
+	public static String lookupString(String name)
+	{
+		return lookupString(name, null);
+	}
 }
