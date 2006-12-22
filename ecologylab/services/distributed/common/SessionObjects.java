173f498dfe06a69118cf39403aadf9cb283a7b7c
toupsz <toupsz@ecologylab.net>
2006-12-22 02:45:14 +0000

    add MAIN_SHUTDOWNABLE & clean up formatting.

--- ecologylab/services/distributed/common/SessionObjects.java
+++ ecologylab/services/distributed/common/SessionObjects.java
@@ -3,19 +3,26 @@ package ecologylab.services;
 import ecologylab.xml.TranslationSpace;
 
 /**
- * Constants that define general ecologylab objects that get stored in the Session ObjectRegistry.
+ * Constants that define general ecologylab objects that get stored in the
+ * Session ObjectRegistry.
+ * 
  * @author andruid
- *
+ * 
  */
 public interface SessionObjects
 {
-	public static final	String		MAIN_START_AND_STOPPABLE	= "main_start_and_stoppable";
-	
-	public static final TranslationSpace	BROWSER_SERVICES_TRANSLATIONS = TranslationSpace.get("Browse", "ecologylab.services.messages");
+    public static final String           MAIN_START_AND_STOPPABLE      = "main_start_and_stoppable";
 
-	public static final String	BROWSER_SERVICES_CLIENT	= "browser_services_client";
-	
-	public static final String LOGGING = "logging";
-	
-	public static final String	TOP_LEVEL	= "top_level";
+    public static final String           MAIN_SHUTDOWNABLE             = "main_shutdownable";
+
+    public static final TranslationSpace BROWSER_SERVICES_TRANSLATIONS = TranslationSpace
+                                                                               .get(
+                                                                                       "Browse",
+                                                                                       "ecologylab.services.messages");
+
+    public static final String           BROWSER_SERVICES_CLIENT       = "browser_services_client";
+
+    public static final String           LOGGING                       = "logging";
+
+    public static final String           TOP_LEVEL                     = "top_level";
 }
