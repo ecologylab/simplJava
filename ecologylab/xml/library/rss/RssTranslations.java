a4a747d552d2ac676ca81b46402c2bceb33de39e
andruid <andruid@ecologylab.net>
2007-02-13 08:30:41 +0000

    Add testing code.

--- ecologylab/xml/library/rss/RssTranslations.java
+++ ecologylab/xml/library/rss/RssTranslations.java
@@ -1,7 +1,10 @@
 package ecologylab.xml.library.rss;
 
 import ecologylab.generic.Debug;
+import ecologylab.net.ParsedURL;
+import ecologylab.xml.ElementState;
 import ecologylab.xml.TranslationSpace;
+import ecologylab.xml.XmlTranslationException;
 import ecologylab.xml.library.dc.Dc;
 
 /**
@@ -10,6 +13,7 @@ import ecologylab.xml.library.dc.Dc;
  * @author andruid
  */
 public class RssTranslations
+extends Debug
 {
    private static final String TRANSLATION_SPACE_NAME	= "rss";
    private static final String PACKAGE_NAME				= "ecologylab.xml.library.rss";
@@ -36,4 +40,24 @@ public class RssTranslations
    {
 	   return TranslationSpace.get(TRANSLATION_SPACE_NAME, PACKAGE_NAME, TRANSLATIONS);
    }
+   
+	
+	static final ParsedURL FLICKR_PURL	= ParsedURL.getAbsolute("http://www.flickr.com/services/feeds/photos_public.gne?format=rss_200&tags=freedom", "");
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			RssState rssState	= (RssState) ElementState.translateFromXML(FLICKR_PURL, get());
+			
+			String xml			= rssState.translateToXML(false);
+			
+			println(xml);
+			
+		} catch (XmlTranslationException e)
+		{
+			e.printStackTrace();
+		}
+	}
+  
 }
