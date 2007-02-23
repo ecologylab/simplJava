857df2c3313a4bac6adc9b6680dd77072dbae296
andruid <andruid@ecologylab.net>
2007-02-23 04:21:03 +0000

    *** empty log message ***

--- ecologylab/xml/types/element/HashMapState.java
+++ ecologylab/xml/types/element/HashMapState.java
@@ -55,7 +55,6 @@ public class HashMapState<K, V extends ElementState & Mappable<K>> extends Eleme
     @Override
     protected Object clone() throws CloneNotSupportedException
     {
-        // TODO Auto-generated method stub
         return super.clone();
     }
 
