858e161a22c8b6c0c11cdcb1666e4e21f114a0b2
andruid <andruid@ecologylab.net>
2007-03-13 20:29:10 +0000

    Initial moves toward generic support with Comparable.

--- ecologylab/appframework/types/prefs/RangeState.java
+++ ecologylab/appframework/types/prefs/RangeState.java
@@ -2,13 +2,20 @@ package ecologylab.appframework.types.prefs;
 
 import ecologylab.xml.ElementState;
 
-abstract public class RangeState<T> extends ElementState
+abstract public class RangeState<T extends Comparable> extends ElementState
 {
-
+	T	min;
+	T	max;
+	
 	public RangeState()
 	{
 		super();
 	}
 
 	abstract protected boolean isWithinRange(T newValue);
+/*	protected boolean isWithinRange(T newValue)
+	{
+		return (newValue >= min) && (newValue <= max);
+	}
+ */
 }
