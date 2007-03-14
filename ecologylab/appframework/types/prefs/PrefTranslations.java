9456b8be3c07fd0ccf59fc84613a7dca5496db0f
andruid <andruid@ecologylab.net>
2007-03-14 17:23:49 +0000

    Name all classes explicitly.

--- ecologylab/appframework/types/prefs/PrefTranslations.java
+++ ecologylab/appframework/types/prefs/PrefTranslations.java
@@ -10,7 +10,7 @@ import ecologylab.xml.library.rss.Channel;
 import ecologylab.xml.library.rss.Item;
 import ecologylab.xml.library.rss.RDFState;
 import ecologylab.xml.library.rss.RssState;
-
+import ecologylab.services.messages.*;
 /**
  * Translations for the pref/meta_pref system.
  * 
@@ -19,16 +19,31 @@ import ecologylab.xml.library.rss.RssState;
  */
 public class PrefTranslations extends Debug 
 {
-
-	private static final String TRANSLATION_SPACE_NAME	= "prefs";
 	private static final String PACKAGE_NAME			= "ecologylab.appframework.types.prefs";
 
 	public static final Class TRANSLATIONS[]	= 
 	{
 		MetaPref.class,
 		MetaPrefSet.class,
+		MetaPrefBoolean.class,
+		MetaPrefFloat.class,
+		MetaPrefInt.class,
+		MetaPrefString.class,
+		
 		Pref.class,
         PrefSet.class,
+        PrefBoolean.class,
+        PrefFloat.class,
+        PrefInt.class,
+        PrefString.class,
+        PrefElementState.class,
+        
+        RangeState.class,
+        RangeIntState.class,
+        
+        Choice.class,
+        ChoiceInt.class,
+        
 		
 	};
 
@@ -41,7 +56,7 @@ public class PrefTranslations extends Debug
 
 	public static TranslationSpace get()
 	{
-		return TranslationSpace.get(TRANSLATION_SPACE_NAME, PACKAGE_NAME, TRANSLATIONS);
+		return TranslationSpace.get(PACKAGE_NAME, TRANSLATIONS);
 	}
 
 }
