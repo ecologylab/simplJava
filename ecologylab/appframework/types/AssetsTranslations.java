38ca19bad2a4a1cf608c4826eb2f5efeb29ef010
andruid <andruid@ecologylab.net>
2007-03-14 17:14:12 +0000

    Add JavaDoc and simplify.

--- ecologylab/appframework/types/AssetsTranslations.java
+++ ecologylab/appframework/types/AssetsTranslations.java
@@ -7,13 +7,13 @@ import ecologylab.generic.Debug;
 import ecologylab.xml.TranslationSpace;
 
 /**
+ * Simple translations used just for loading the assets.xml file.
+ * 
  * @author robinson
- *
+ * @author andruid
  */
 public class AssetsTranslations extends Debug 
 {
-	public static final String	NAME			= "ecologylab.assets.types";
-	
 	public static final String	PACKAGE_NAME	= "ecologylab.assets.types";
 	
 	public static final Class	TRANSLATIONS[]	= 
@@ -23,10 +23,13 @@ public class AssetsTranslations extends Debug
 		
 	};
 	
-	
+	/**
+	 * Get existing TranslationSpace with this name, or create a new one, and map it.
+	 * 
+	 * @return	TranslationSpace with simple translations used just for loading the assets.xml file.
+	 */
 	public static TranslationSpace get()
 	{
-		return TranslationSpace.get(NAME, PACKAGE_NAME, TRANSLATIONS);
+		return TranslationSpace.get(PACKAGE_NAME, TRANSLATIONS);
 	}
-
 }
