ebcd40e7566564aa3b0538ea30999dee9ea64603
andruid <andruid@ecologylab.net>
2007-09-19 20:42:53 +0000

    Change StringBuffer to StringBuilder

--- ecologylab/generic/StringBuffersPool.java
+++ ecologylab/generic/StringBuffersPool.java
@@ -4,50 +4,50 @@ package ecologylab.generic;
 import java.util.*;
 
 /**
- * A pool of reusable {@link java.lang.StringBuffer StringBuffer}s.
+ * A pool of reusable {@link java.lang.StringBuilder StringBuilder}s.
  */
 public class StringBuffersPool
 extends Debug
 {
 	public static int DEFAULT_POOL_SIZE	=	64;
-	public Vector bufferPool;
+	public Vector<StringBuilder> bufferPool;
 	
 	int	bufferSize;
 	int	poolSize;
 	
 	public StringBuffersPool(int bufferSize)
 	{
-	   this(bufferSize, DEFAULT_POOL_SIZE);
+		this(bufferSize, DEFAULT_POOL_SIZE);
 	}
 	public StringBuffersPool(int bufferSize, int poolSize)
 	{
-	   this.bufferSize	= bufferSize;
-	   bufferPool		= new Vector(poolSize);
-	   for(int i = 0 ; i < poolSize; i++)
-	   {
-	      bufferPool.add(new StringBuffer(bufferSize));
-	   }	
+		this.bufferSize	= bufferSize;
+		bufferPool		= new Vector<StringBuilder>(poolSize);
+		for(int i = 0 ; i < poolSize; i++)
+		{
+			bufferPool.add(new StringBuilder(bufferSize));
+		}	
 	}
 	
-	public StringBuffer nextBuffer()
+	public StringBuilder nextBuffer()
 	{
-	   synchronized (bufferPool)
-	   {
-	      int freeIndex = bufferPool.size() - 1;
-	      if (freeIndex == -1)
-	      {
-		 debug("extending pool??!");
-		 return (new StringBuffer(bufferSize));
-	      }
-	      StringBuffer b = (StringBuffer) bufferPool.remove(freeIndex);
-	      return b;
-	   }
+		synchronized (bufferPool)
+		{
+			int freeIndex = bufferPool.size() - 1;
+			if (freeIndex == -1)
+			{
+				debug("extending pool??!");
+				return (new StringBuilder(bufferSize));
+			}
+			StringBuilder b = (StringBuilder) bufferPool.remove(freeIndex);
+			return b;
+		}
 	}
 	
-	public void release(StringBuffer b)
+	public void release(StringBuilder b)
 	{
-//	   b.setLength(0); // UGH! actually reallocates due to Sun stupidity!
-	   b.delete(0, b.length());
-	   bufferPool.add(b);		
+//		b.setLength(0); // UGH! actually reallocates due to Sun stupidity!
+		b.delete(0, b.length());
+		bufferPool.add(b);		
 	}
 }
