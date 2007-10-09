c51a79ecb81d70fcc20becbde8a55277c9191a24
andruid <andruid@ecologylab.net>
2007-10-09 00:36:22 +0000

    Add ROOT type.

--- ecologylab/xml/ParseTableEntryTypes.java
+++ ecologylab/xml/ParseTableEntryTypes.java
@@ -2,40 +2,44 @@ package ecologylab.xml;
 
 public interface ParseTableEntryTypes
 {
-	public static int	UNSET_TYPE				= -99;
+	public static final int	UNSET_TYPE				= -99;
 	/**
 	 * This means we experienced an error while parsing.
 	 * This should never happen.
 	 */
-	public static int	BAD_FIELD				= -1;
+	public static final int	BAD_FIELD				= -1;
 	
 	/**
 	 * This means that we don't bother to parse the element,
 	 * because the programmer developing ElementState subclasses did not
 	 * bother to create fields that use it.
 	 */
-	public static int	IGNORED_ELEMENT			= 0;
+	public static final int	IGNORED_ELEMENT			= 0;
 
-	public static int	REGULAR_NESTED_ELEMENT	= 1;
+	public static final int	REGULAR_NESTED_ELEMENT	= 1;
 
-	public static int	LEAF_NODE_VALUE			= 2;
+	public static final int	LEAF_NODE_VALUE			= 2;
 	
-	public static int	COLLECTION_ELEMENT		= 3;
+	public static final int	COLLECTION_ELEMENT		= 3;
 	
-	public static int	COLLECTION_SCALAR		= 4;
+	public static final int	COLLECTION_SCALAR		= 4;
 	
-	public static int	MAP_ELEMENT				= 5;
+	public static final int	MAP_ELEMENT				= 5;
 	
-	public static int	MAP_SCALAR				= 6;
+	public static final int	MAP_SCALAR				= 6;
 	
-	public static int	REGULAR_ATTRIBUTE		= 10;
+	public static final int	REGULAR_ATTRIBUTE		= 10;
 	
-	public static int	IGNORED_ATTRIBUTE		= 0;
+	public static final int	IGNORED_ATTRIBUTE		= 0;
 	
 	/**
 	 * These will be handled by otherNestedElement().
 	 */
-	public static int	OTHER_NESTED_ELEMENT	= 9;
+	public static final int	OTHER_NESTED_ELEMENT	= 9;
 
-
+	
+	/**
+	 * Root node type: for TagMapEntry translateTo only.
+	 */
+	public static final int	ROOT					= 0X20;
 }
