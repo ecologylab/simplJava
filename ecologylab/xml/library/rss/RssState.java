0ba7a986c9e9c3e0e7b61ef9a516f6a81e8794bb
andruid <andruid@ecologylab.net>
2007-11-05 19:09:44 +0000

    Make it a little nicer.

--- ecologylab/xml/library/rss/RssState.java
+++ ecologylab/xml/library/rss/RssState.java
@@ -1,5 +1,7 @@
 package ecologylab.xml.library.rss;
 
+import java.io.File;
+
 import ecologylab.net.ParsedURL;
 import ecologylab.xml.*;
 
@@ -49,11 +51,20 @@ public class RssState extends ElementState
 	
 	public static final ParsedURL NYT_TECH_FEED	= ParsedURL.getAbsolute("http://www.nytimes.com/services/xml/rss/nyt/Technology.xml");
 	
+	public static final ParsedURL CNN_WORLD_FEED	= ParsedURL.getAbsolute("http://rss.cnn.com/rss/cnn_world.rss");
+	
+	public static final ParsedURL BBC_FRONT_FEED	= ParsedURL.getAbsolute("http://news.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml");
+	
+	public static final File 	outputFile			= new File("/temp/rss.xml");
 	public static void main(String[] args)
 	{
 		try
 		{
-			RssState rssState	= (RssState) ElementState.translateFromXML(NYT_TECH_FEED, RssTranslations.get());
+			ParsedURL feedPURL	= BBC_FRONT_FEED;
+			println("Translating RSS feed: " + feedPURL+"\n");
+
+//			RssState rssState	= (RssState) ElementState.translateFromXMLSAX(feedPURL, RssTranslations.get());
+			RssState rssState	= (RssState) ElementState.translateFromXML(feedPURL, RssTranslations.get());
 			println("items: " + rssState.getChannel()/*.getItems() */.size());
 			
 			String retranslated	= rssState.translateToXML();
@@ -62,6 +73,7 @@ public class RssState extends ElementState
 			RssState rssState2	= (RssState) ElementState.translateFromXMLString(retranslated, RssTranslations.get());
 			String reretranslated	= rssState2.translateToXML();
 			println(reretranslated);
+			rssState.savePrettyXML(outputFile);
 		} catch (XmlTranslationException e)
 		{
 			// TODO Auto-generated catch block
