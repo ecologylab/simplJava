69fe9e152d60f5cd2a2b790146c3901786b94162
andruid <andruid@ecologylab.net>
2007-11-06 01:18:41 +0000

    Use @xml_tag at class level.

--- ecologylab/tests/Composed.java
+++ ecologylab/tests/Composed.java
@@ -6,12 +6,14 @@ package ecologylab.tests;
 import ecologylab.xml.ElementState;
 import ecologylab.xml.TranslationSpace;
 import ecologylab.xml.XmlTranslationException;
+import ecologylab.xml.ElementState.xml_tag;
 import ecologylab.xml.types.element.ArrayListState;
 
 /**
  * @author toupsz
  *
  */
+@xml_tag("fred") 
 public class Composed extends ElementState
 {
     @xml_nested ArrayListState<ClassTagged> tagged = new ArrayListState<ClassTagged>();
@@ -25,16 +27,21 @@ public class Composed extends ElementState
         tagged.add(new ClassTagged());
         tagged.add(new ClassTagged());
     }
+    static final Class[] classes = {Composed.class, ClassTagged.class, FieldTagged.class};
 
     public static void main(String[] args) throws XmlTranslationException
     {
+        TranslationSpace ts = TranslationSpace.get("testXMLTag", classes);
+        
         Composed c = new Composed();
         
         System.out.println(c.translateToXML());
         
-        Class[] classes = {Composed.class, ClassTagged.class, FieldTagged.class};
-        TranslationSpace ts = TranslationSpace.get("testXMLTag", classes);
+         Composed composed	= (Composed) ElementState.translateFromXMLString(c.translateToXML(), ts);
         
-        System.out.println(((Composed) ElementState.translateFromXMLString(c.translateToXML(), ts)).translateToXML());
+        c.translateToXML(System.out);
+        System.out.println("\n");
+        
+        composed.writePrettyXML(System.out);
     }
 }
