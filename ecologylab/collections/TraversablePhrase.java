9f97c6ac6e08f0c7822132d3bd4490e7e0b9821e
andruid <andruid@ecologylab.net>
2008-01-26 01:16:55 +0000

    Start of an idea.

---
+++ ecologylab/collections/TraversablePhrase.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ecologylab.collections;
+
+import java.util.HashMap;
+
+import ecologylab.generic.Debug;
+
+/**
+ * Recursive unit for url portion pattern matching.
+ * 
+ * @author andruid
+ *
+ */
+public class TraversablePhrase extends HashMap<String, TraversablePhrase>
+{
+
+	/**
+	 * 
+	 */
+	public TraversablePhrase()
+	{
+	}
+
+}
