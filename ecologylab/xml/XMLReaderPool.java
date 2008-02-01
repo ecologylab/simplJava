536d65940930e5557e65f69ef08e75771c1cf708
toupsz <toupsz@ecologylab.net>
2008-02-01 03:35:22 +0000

    add XMLReaderPool

---
+++ ecologylab/xml/XMLReaderPool.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package ecologylab.xml;
+
+import ecologylab.generic.ResourcePool;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+/**
+ * @author toupsz
+ *
+ */
+public class XMLReaderPool extends ResourcePool<XMLReader>
+{
+
+	/**
+	 * @param instantiateResourcesInPool
+	 * @param initialPoolSize
+	 * @param minimumPoolSize
+	 */
+	public XMLReaderPool(boolean instantiateResourcesInPool,
+			int initialPoolSize, int minimumPoolSize)
+	{
+		super(instantiateResourcesInPool, initialPoolSize, minimumPoolSize);
+	}
+
+	/**
+	 * @param initialPoolSize
+	 * @param minimumPoolSize
+	 */
+	public XMLReaderPool(int initialPoolSize, int minimumPoolSize)
+	{
+		super(initialPoolSize, minimumPoolSize);
+	}
+
+	/**
+	 * Does nothing; XMLReaders CANNOT be cleaned.
+	 * 
+	 * @see ecologylab.generic.ResourcePool#clean(java.lang.Object)
+	 */
+	@Override protected void clean(XMLReader objectToClean)
+	{
+	}
+
+	/**
+	 * @see ecologylab.generic.ResourcePool#generateNewResource()
+	 */
+	@Override protected XMLReader generateNewResource()
+	{
+		try
+		{
+			return ElementStateSAXHandler.createXMLReader();
+		}
+		catch (SAXException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
