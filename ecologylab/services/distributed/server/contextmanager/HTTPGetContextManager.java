c7da2cd81091853951eb915348d05155d9481382
awebb <awebb@ecologylab.net>
2008-02-05 17:34:33 +0000

    added class HTTPContextManager and HTTPPostCOntextManager. HTTPContextManager contains code used by both HTTPGet and HTTPPost ContextManagers.

--- ecologylab/services/distributed/server/contextmanager/HTTPGetContextManager.java
+++ ecologylab/services/distributed/server/contextmanager/HTTPGetContextManager.java
@@ -8,7 +8,6 @@ import java.net.URLDecoder;
 import java.nio.channels.SelectionKey;
 
 import ecologylab.appframework.ObjectRegistry;
-import ecologylab.net.ParsedURL;
 import ecologylab.services.distributed.impl.NIOServerBackend;
 import ecologylab.services.distributed.server.NIOServerFrontend;
 import ecologylab.services.messages.HttpGetRequest;
@@ -23,22 +22,10 @@ import ecologylab.xml.XMLTranslationException;
  * @author Zachary O. Toups (toupsz@cs.tamu.edu)
  * 
  */
-public class HTTPGetContextManager extends AbstractContextManager
+public class HTTPGetContextManager extends HTTPContextManager
 {
-	static final String				HTTP_PREPEND					= "GET /";
-
-	static final int					HTTP_PREPEND_LENGTH			= HTTP_PREPEND.length();
-
-	private static final String	HTTP_VERSION					= "HTTP/1.1";
-
-	private static final String	HTTP_RESPONSE_HEADERS		= HTTP_VERSION + " 307 Temporary Redirect" + "\r\n"
-																						+ "Date: Fri, 17 Nov 2006 05:21:59 GMT\r\n";
-
-	static final String				HTTP_APPEND						= " " + HTTP_VERSION;
-
-	static final int					HTTP_APPEND_LENGTH			= HTTP_APPEND.length();
-
-	protected boolean					ALLOW_HTTP_STYLE_REQUESTS	= true;
+	static final String	HTTP_PREPEND		= "GET /";
+	static final int	HTTP_PREPEND_LENGTH	= HTTP_PREPEND.length();
 
 	/**
 	 * @param token
@@ -88,54 +75,6 @@ public class HTTPGetContextManager extends AbstractContextManager
 		return super.translateStringToRequestMessage(messageString);
 	}
 
-	/**
-	 * @see ecologylab.services.distributed.server.contextmanager.ContextManager#clearOutgoingMessageHeaderBuffer(java.lang.StringBuilder)
-	 */
-	@Override protected void clearOutgoingMessageHeaderBuffer(StringBuilder outgoingMessageHeaderBuf)
-	{
-		outgoingMessageHeaderBuf.delete(0, outgoingMessageHeaderBuf.length());
-	}
-
-	/**
-	 * @see ecologylab.services.distributed.server.contextmanager.ContextManager#createHeader(java.lang.StringBuilder,
-	 *      java.lang.StringBuilder, RequestMessage, ResponseMessage)
-	 */
-	@Override protected void createHeader(StringBuilder outgoingMessageBuf, StringBuilder outgoingMessageHeaderBuf,
-			RequestMessage incomingRequest, ResponseMessage outgoingResponse, long uid)
-	{
-		if (incomingRequest instanceof HttpGetRequest)
-		{
-			HttpGetRequest httpRequest = (HttpGetRequest) incomingRequest;
-
-			ParsedURL responseUrl = null;
-			if (outgoingResponse.isOK())
-				responseUrl = httpRequest.okResponseUrl();
-			else
-				responseUrl = httpRequest.errorResponseUrl();
-
-			debugA("responseUrl: " + responseUrl);
-
-			if (responseUrl != null)
-				outgoingMessageHeaderBuf.append(HTTP_RESPONSE_HEADERS + "Location: " + responseUrl.toString() + "\r\n\r\n");
-
-			debugA("Server sending response!!!\n" + outgoingMessageHeaderBuf.toString());
-		}
-	}
-
-	/**
-	 * @see ecologylab.services.distributed.server.contextmanager.AbstractContextManager#clearOutgoingMessageBuffer(java.lang.StringBuilder)
-	 */
-	@Override protected void clearOutgoingMessageBuffer(StringBuilder outgoingMessageBuf)
-	{
-	}
-
-	/**
-	 * @see ecologylab.services.distributed.server.contextmanager.AbstractContextManager#prepareBuffers(java.lang.StringBuilder,
-	 *      java.lang.StringBuilder, java.lang.StringBuilder)
-	 */
-	@Override protected void prepareBuffers(StringBuilder incomingMessageBuf, StringBuilder outgoingMessageBuf,
-			StringBuilder outgoingMessageHeaderBuf)
-	{
-	}
+	
 
 }
