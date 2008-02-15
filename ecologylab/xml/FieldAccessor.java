fac31362da641c6d4bb80ce4b143062779ab02f4
andruid <andruid@ecologylab.net>
2008-02-15 02:26:33 +0000

    For fields in an ElementState, provides programmatic marshalling and unmarshalling of values in conjunction with get/set methods.

---
+++ ecologylab/xml/FieldAccessor.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package ecologylab.xml;
+
+import java.lang.reflect.Field;
+
+import ecologylab.generic.Debug;
+import ecologylab.xml.types.scalar.ScalarType;
+
+/**
+ * Used to provide convenient access for setting and getting values, using the ecologylab.xml type system.
+ * Provides marshalling and unmarshalling from Strings.
+ * 
+ * @author andruid
+ */
+public class FieldAccessor extends Debug
+{
+	final Field				field;
+	final ScalarType<?>		scalarType;
+	
+	public FieldAccessor(Field field, ScalarType<?> scalarType)
+	{
+		this.field		= field;
+		this.scalarType	= scalarType;
+	}
+	
+	/**
+	 * 
+	 * @return	true if this field represents a ScalarType, not a nested element or collection thereof.
+	 */
+	public boolean isScalar()
+	{
+		return scalarType != null;
+	}
+	
+	/**
+	 * In the supplied context object, set the *typed* value of the field,
+	 * using the valueString passed in. 
+	 * Unmarshalling is performed automatically, by the ScalarType already stored in this.
+	 * 
+	 * @param context			ElementState object to set the Field in this.
+	 * 
+	 * @param valueString		The value to set, which this method will use with the ScalarType, to create the value that will be set.
+	 */
+	public void set(ElementState context, String valueString)
+	{
+		if ((valueString != null) && (context != null))
+		{
+			if (isScalar())
+			{
+				scalarType.setField(context, field, valueString);
+			}
+		}
+	}
+	
+	/**
+	 * Get the String representation of the value of the field, in the context object, using the ScalarType.
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public String getValueString(ElementState context)
+	{
+		String	result	= null;
+		if (context != null)
+		{
+			if (isScalar())
+			{
+				result		= scalarType.toString(field, context);
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @return	The Java name of the field.
+	 */
+	public String fieldName()
+	{
+		return field.getName();
+	}
+}
+
