4a881275a8db6c35c71075fcf3535eae5dded77a
toupsz <toupsz@ecologylab.net>
2008-10-09 18:26:07 +0000

    improve efficiency for collision detection, goal collection, GPS computation, and WiFi computation by partitioning the game space and pre-computing each partition
    update scoring algorithm to increase value of multi-seeker goals
    change score display to highlight large increases in score
    adjust messages to display correct data
    
    make sure that seekers "release" goals when they are killed while collecting
    
    fix a bug with the tutorial mode where wrong goals appear
    
    add silencing switch to the patch for the radio

--- ecologylab/xml/library/geom/RectangularShape.java
+++ ecologylab/xml/library/geom/RectangularShape.java
@@ -9,82 +9,83 @@ import ecologylab.xml.ElementState;
 
 public abstract class RectangularShape extends ElementState implements Shape
 {
-    /**
-     * Location and dimensions of the ellipse.
-     */
-    protected @xml_attribute double              x         = 0;
+	/**
+	 * Location and dimensions of the ellipse.
+	 */
+	protected @xml_attribute double	x	= 0;
 
-    protected @xml_attribute double              y         = 0;
+	protected @xml_attribute double	y	= 0;
 
-    protected @xml_attribute double              w         = 0;
+	protected @xml_attribute double	w	= 0;
 
-    protected @xml_attribute double              h         = 0;   
-    
-    public RectangularShape()
-    {
-        super();
-    }
-    
-    public RectangularShape(double x, double y, double width, double height)
-    {
-        super();
-        
-        setFrame(x, y, width, height);
-    }
-    
-    public abstract java.awt.geom.RectangularShape shape();
-    
-    public void setFrame(double x, double y, double w, double h)
-    {
-        this.x = x;
-        this.y = y;
-        // if we have a negative width or height, we assume the rectangle should just be translated to ensure that w and h are always positive
-        if (w < 0)
-        {
-            x += w;
-            w *= -1;
-        }
+	protected @xml_attribute double	h	= 0;
 
-        this.w = w;
-        
-        if (h < 0)
-        {
-            y += h;
-            h *= -1;
-        }
-    
-        this.h = h;
-    }
+	public RectangularShape()
+	{
+		super();
+	}
 
-    /**
-     * @return the h
-     */
-    public double getHeight()
-    {
-        return h;
-    }
+	public RectangularShape(double x, double y, double width, double height)
+	{
+		super();
 
-    /**
-     * @return the w
-     */
-    public double getWidth()
-    {
-        return w;
-    }
+		setFrame(x, y, width, height);
+	}
 
-    /**
-     * @return the x
-     */
-    public double getX()
-    {
-        return x;
-    }
+	public abstract java.awt.geom.RectangularShape shape();
 
-    /**
-     * @return the y
-     */
-    public double getY()
-    {
-        return y;
-    }
+	public void setFrame(double x, double y, double w, double h)
+	{
+		this.x = x;
+		this.y = y;
+		// if we have a negative width or height, we assume the rectangle should
+		// just be translated to ensure that w and h are always positive
+		if (w < 0)
+		{
+			x += w;
+			w *= -1;
+		}
+
+		this.w = w;
+
+		if (h < 0)
+		{
+			y += h;
+			h *= -1;
+		}
+
+		this.h = h;
+	}
+
+	/**
+	 * @return the h
+	 */
+	public double getHeight()
+	{
+		return h;
+	}
+
+	/**
+	 * @return the w
+	 */
+	public double getWidth()
+	{
+		return w;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public double getX()
+	{
+		return x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public double getY()
+	{
+		return y;
+	}
 }
