b57a8f7b7649c67d72ed5b34fae5f1809445ebe4
andruid <andruid@ecologylab.net>
2009-05-29 01:15:55 +0000

    Move code around to improve the structure of everything. Eliminate unnecessary references.

---
+++ ecologylab/textformat/ScaledValueObserver.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Sep 28, 2005
+ * 
+ */
+package ecologylab.textformat;
+
+import java.util.Observer;
+
+/**
+ * @author Andrew Webb
+ *
+ */
+public interface ScaledValueObserver extends Observer 
+{
+	public short getScaledValue();
+}
