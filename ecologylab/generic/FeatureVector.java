d6d32ca090e5f615f3670ef2aeeedbe422a1f1cc
andruid <andruid@ecologylab.net>
2009-07-14 19:06:52 +0000

    Avoid NullPointer in recycle().

--- ecologylab/generic/FeatureVector.java
+++ ecologylab/generic/FeatureVector.java
@@ -353,8 +353,11 @@ public class FeatureVector<T> extends Observable implements IFeatureVector<T>
 	
 	public void recycle()
 	{
-		values.clear();
-		values	= null;//TODO could drop this line to be more moderate
+		if (values != null)
+		{
+			values.clear();
+			values	= null;//TODO could drop this line to be more moderate
+		}
 	}
 	
 }
