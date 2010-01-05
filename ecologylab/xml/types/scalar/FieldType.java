f71675fb48ecb701a03164f03bbe306c5a334bd3
andruid <andruid@ecologylab.net>
2010-01-05 02:06:56 +0000

    For marshalling ecologylab.xml basics -- TranslationScope, ClassDescriptor, FieldDescriptor -- for ports to new platforms.

---
+++ ecologylab/xml/types/scalar/FieldType.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package ecologylab.xml.types.scalar;
+
+import java.awt.Color;
+import java.lang.reflect.Field;
+
+import ecologylab.xml.ScalarUnmarshallingContext;
+
+/**
+ * @author andruid
+ *
+ */
+public class FieldType extends ReferenceType<Field>
+{
+
+	public FieldType()
+	{
+		super(Field.class);
+	}
+
+	/**
+	 * In Java it is not possible to create instances of Field.
+	 */
+	@Override
+	public Field getInstance(String value, String[] formatStrings,
+			ScalarUnmarshallingContext scalarUnmarshallingContext)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Used to describe scalar types used for serializing the type system, itself.
+	 * They cannot be unmarshalled in Java, only marshalled.
+	 * Code may be written to access their String representations in other languages.
+	 * 
+	 * @return	true, because this type cannot be unmarshalled in Java.
+	 */
+	public boolean isMarshallOnly()
+	{
+		return true;
+	}
+
+	/**
+	 * The string representation for a Field of the type Field, used for marshalling.
+	 */
+		@Override
+		public String marshall(Field instance)
+		{
+			return instance.getName();
+		}
+
+}
