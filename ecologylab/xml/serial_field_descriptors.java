347090ef439ff4c1e73491efa6519d352e920eb0
andruid <andruid@ecologylab.net>
2010-05-19 20:46:31 +0000

    New annotations for leveraging s.im.pl serialization transparency.

---
+++ ecologylab/xml/serial_field_descriptors.java
@@ -0,0 +1,24 @@
+package ecologylab.xml;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Class-level annotation.
+ * S.IM.PL Serialization transparency. 
+ * Enables overriding the class used for the FieldDescriptors for fields inside a particular type.
+ * 
+ * @author andruid
+ */
+
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface serial_field_descriptors
+{
+  Class value();
+}
