2468656d6aed5454e8c91bdaf75234747a559b32
toupsz <toupsz@ecologylab.net>
2010-07-07 16:50:12 +0000

    add test case for XML_Text

--- ecologylab/xml/library/jnlp/information/Description.java
+++ ecologylab/xml/library/jnlp/information/Description.java
@@ -58,14 +58,4 @@ public class Description extends ElementState implements Mappable<String>
     {
         return desc;
     }
-
-    public static void main(String[] args) throws SIMPLTranslationException
-    {
-    	String descChunk = "<description kind=\"short\">A demo of the capabilities of the Swing Graphical User Interface.</description>";
-    	
-    	Description descObj = (Description) ElementState.translateFromXMLCharSequence(descChunk, TranslationScope.get("JNLPDesc", Description.class));
-    	
-    	System.out.println(descChunk);
-    	descObj.serialize(System.out);
-    }
 }
