e38e2180c7a7450d64f9788f628a60cdd7e66acb
toupsz <toupsz@ecologylab.net>
2010-07-07 16:45:38 +0000

    add test case

--- ecologylab/xml/library/jnlp/information/Description.java
+++ ecologylab/xml/library/jnlp/information/Description.java
@@ -5,6 +5,8 @@ package ecologylab.xml.library.jnlp.information;
 
 import ecologylab.xml.ElementState;
 import ecologylab.xml.Hint;
+import ecologylab.xml.SIMPLTranslationException;
+import ecologylab.xml.TranslationScope;
 import ecologylab.xml.types.element.Mappable;
 
 /**
@@ -57,4 +59,13 @@ public class Description extends ElementState implements Mappable<String>
         return desc;
     }
 
+    public static void main(String[] args) throws SIMPLTranslationException
+    {
+    	String descChunk = "<description kind=\"short\">A demo of the capabilities of the Swing Graphical User Interface.</description>";
+    	
+    	Description descObj = (Description) ElementState.translateFromXMLCharSequence(descChunk, TranslationScope.get("JNLPDesc", Description.class));
+    	
+    	System.out.println(descChunk);
+    	descObj.serialize(System.out);
+    }
 }
