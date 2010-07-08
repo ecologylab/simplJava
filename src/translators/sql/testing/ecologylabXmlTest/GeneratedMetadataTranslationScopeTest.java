e9abe4cd3dd8913bcd8cf1a9a971527a76f98a7a
hyunjun <hyunjun@ecologylab.net>
2010-07-08 17:34:48 +0000

    sample test class

---
+++ src/translators/sql/testing/ecologylabXmlTest/GeneratedMetadataTranslationScopeTest.java
@@ -0,0 +1,168 @@
+package translators.sql.testing.ecologylabXmlTest;
+
+/**
+This is a generated code. DO NOT edit or modify it.
+ @author MetadataCompiler 
+
+**/ 
+
+
+
+import ecologylab.generic.HashMapArrayList;
+import ecologylab.net.ParsedURL;
+import ecologylab.semantics.generated.library.*;
+import ecologylab.semantics.library.flickr.*;
+import ecologylab.semantics.library.imdb.*;
+import ecologylab.semantics.library.scholarlyPublication.*;
+import ecologylab.semantics.library.uva.*;
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.MetadataBuiltinsTranslationScope;
+import ecologylab.semantics.metadata.builtins.*;
+import ecologylab.semantics.metadata.builtins.DebugMetadata;
+import ecologylab.semantics.metadata.builtins.Document;
+import ecologylab.semantics.metadata.builtins.Entity;
+import ecologylab.semantics.metadata.builtins.Image;
+import ecologylab.semantics.metadata.builtins.Media;
+import ecologylab.semantics.metadata.scalar.*;
+import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.serialization.ElementState.xml_tag;
+import ecologylab.serialization.Hint;
+import ecologylab.serialization.TranslationScope;
+import ecologylab.serialization.simpl_inherit;
+import ecologylab.serialization.types.element.Mappable;
+import java.util.*;
+
+
+/**
+	
+This is the tranlation scope class for generated files
+.
+**/ 
+
+public class GeneratedMetadataTranslationScopeTest
+{protected static final Class TRANSLATIONS[]=
+	{Pdf.class,
+
+AcmProceeding.class,
+
+MmArtwork.class,
+
+Artwork.class,
+
+HotSearch.class,
+
+GoogleTrends.class,
+
+Bookmark.class,
+
+DeliciousHomepage.class,
+
+FlickrImageDetail.class,
+
+FlickrTag.class,
+
+FlickrImage.class,
+
+FlickrTags.class,
+
+FlickrAuthor.class,
+
+FlickrSearch.class,
+
+FlickrTagsInteresting.class,
+
+FlickrLink.class,
+
+GooglePatent.class,
+
+GoogleSorry.class,
+
+GooglePatentImage.class,
+
+PersonDetails.class,
+
+Genre.class,
+
+CastMember.class,
+
+ImdbTitle.class,
+
+ImdbGenre.class,
+
+ImdbChart.class,
+
+BirthDetail.class,
+
+Lolz.class,
+
+Topic.class,
+
+DcDocument.class,
+
+Icdl.class,
+
+Text.class,
+
+IcdlImage.class,
+
+Dlms.class,
+
+Nsdl.class,
+
+Restaurant.class,
+
+Item.class,
+
+Channel.class,
+
+Item.class,
+
+Rss.class,
+
+Link.class,
+
+Link.class,
+
+ScholarlyArticle.class,
+
+Author.class,
+
+Source.class,
+
+Result.class,
+
+YahooResultSet.class,
+
+Search.class,
+
+SearchResult.class,
+
+Tumblr.class,
+
+WeatherReport.class,
+
+WikipediaPage.class,
+
+Anchor.class,
+
+Paragraph.class,
+
+Category.class,
+
+Thumbinner.class,
+
+Anchor.class,
+
+WikipediaPageType.class,
+
+UrbanSpoonGenre.class,
+
+
+};
+ 
+public static TranslationScope get()
+{
+return TranslationScope.get("generated_metadata_translations", MetadataBuiltinsTranslationScope.get(), TRANSLATIONS);
+}
+}
