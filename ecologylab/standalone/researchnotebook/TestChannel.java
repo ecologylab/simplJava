6115f36c5ca1be55704b57f834684513ca883950
hyunjun <hyunjun@ecologylab.net>
2010-08-03 17:08:54 +0000

    update

--- ecologylab/standalone/researchnotebook/TestChannel.java
+++ ecologylab/standalone/researchnotebook/TestChannel.java
@@ -7,10 +7,10 @@ import ecologylab.serialization.ElementState;
 import ecologylab.serialization.Hint;
 
 public class TestChannel extends ElementState{
-	@simpl_scalar String title;
-//	@simpl_scalar ParsedURL link;
-//	@simpl_scalar String description; 
-//	@simpl_scalar String language; 
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String title;
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) ParsedURL link;
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String description; 
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String language; 
 	
 	@simpl_nowrap
 	@simpl_collection("test_item") ArrayList<TestItem> items = new ArrayList<TestItem>(); 
--- ecologylab/standalone/researchnotebook/TestItem.java
+++ ecologylab/standalone/researchnotebook/TestItem.java
@@ -1,8 +1,16 @@
 package ecologylab.standalone.researchnotebook;
 
+import java.util.Date;
+
+import ecologylab.net.ParsedURL;
 import ecologylab.serialization.ElementState;
+import ecologylab.serialization.Hint;
 
 // TestChannel.java 
 public class TestItem extends ElementState{
-
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String title; 
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) ParsedURL link;
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String description; 
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String guid; 
+	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String pubDate; 
 }
