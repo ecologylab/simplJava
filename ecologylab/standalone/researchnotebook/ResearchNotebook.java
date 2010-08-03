29cd7a08ab09080f674a3a10b8b2d6eb822bd8d4
hyunjun <hyunjun@ecologylab.net>
2010-08-03 16:42:59 +0000

    research notebook main class

---
+++ ecologylab/standalone/researchnotebook/ResearchNotebook.java
@@ -0,0 +1,13 @@
+package ecologylab.standalone.researchnotebook;
+
+public class ResearchNotebook {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
