3e0d9311fc0c849d3e7cd7ea8e5dbe7561495a24
hyunjun <hyunjun@ecologylab.net>
2010-08-03 03:09:42 +0000

    TestXml_Ecologylab subclass

--- ecologylab/standalone/TestItems.java
+++ ecologylab/standalone/TestItems.java
@@ -6,6 +6,17 @@ import ecologylab.serialization.ElementState;
 
 public class TestItems extends ElementState
 {
+	
+	@simpl_nowrap @xml_tag("test_sub_item")
+	@simpl_collection("test_sub_item") ArrayList<String>	country_name = new ArrayList<String>();
+	
+	@simpl_nowrap @xml_tag("test_sub_item")
+	@simpl_collection("test_sub_item") ArrayList<TestSubItem> countries = new ArrayList<TestSubItem>();
+	
 	@simpl_nowrap
-	@simpl_collection("country") ArrayList<String> countries = new ArrayList<String>(); 
+	@simpl_collection("test_sub_item2") ArrayList<TestSubItem2> sub = new ArrayList<TestSubItem2>(); 
+	
+	@simpl_scalar String name; 
+	
+	@simpl_scalar String priority; 
 }
