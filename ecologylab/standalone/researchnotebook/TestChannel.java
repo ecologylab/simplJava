63d6cb911c58b9bf2f0cd95c903fb1942b0e5dc7
hyunjun <hyunjun@ecologylab.net>
2010-08-03 17:25:35 +0000

    adding set and get methods

--- ecologylab/standalone/researchnotebook/TestChannel.java
+++ ecologylab/standalone/researchnotebook/TestChannel.java
@@ -13,5 +13,45 @@ public class TestChannel extends ElementState{
 	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String language; 
 	
 	@simpl_nowrap
-	@simpl_collection("test_item") ArrayList<TestItem> items = new ArrayList<TestItem>(); 
+	@simpl_collection("test_item") ArrayList<TestItem> items = new ArrayList<TestItem>();
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public ParsedURL getLink() {
+		return link;
+	}
+
+	public void setLink(ParsedURL link) {
+		this.link = link;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public ArrayList<TestItem> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<TestItem> items) {
+		this.items = items;
+	} 
 }
--- ecologylab/standalone/researchnotebook/TestItem.java
+++ ecologylab/standalone/researchnotebook/TestItem.java
@@ -12,5 +12,36 @@ public class TestItem extends ElementState{
 	@simpl_scalar @simpl_hints(Hint.XML_LEAF) ParsedURL link;
 	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String description; 
 	@simpl_scalar @simpl_hints(Hint.XML_LEAF) String guid; 
-	@xml_tag("pubDate") @simpl_scalar @simpl_hints(Hint.XML_LEAF) String pubDate; 
+	@xml_tag("pubDate") @simpl_scalar @simpl_hints(Hint.XML_LEAF) String pubDate;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public ParsedURL getLink() {
+		return link;
+	}
+	public void setLink(ParsedURL link) {
+		this.link = link;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getGuid() {
+		return guid;
+	}
+	public void setGuid(String guid) {
+		this.guid = guid;
+	}
+	public String getPubDate() {
+		return pubDate;
+	}
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	} 
 }
