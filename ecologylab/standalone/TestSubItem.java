545fdecb146665de10b4dd1b80496c167f9f5460
hyunjun <hyunjun@ecologylab.net>
2010-08-03 03:09:01 +0000

    TestXml_Ecologylab sub classes

---
+++ ecologylab/standalone/TestSubItem.java
@@ -0,0 +1,11 @@
+package ecologylab.standalone;
+
+import java.util.ArrayList;
+
+import ecologylab.serialization.ElementState;
+
+public class TestSubItem extends ElementState{	
+	@simpl_scalar String priority;
+	
+	@simpl_scalar String target; 
+}
---
+++ ecologylab/standalone/TestSubItem2.java
@@ -0,0 +1,8 @@
+package ecologylab.standalone;
+
+import ecologylab.serialization.ElementState;
+
+public class TestSubItem2 extends ElementState{
+	@simpl_scalar String priority; 
+
+}
