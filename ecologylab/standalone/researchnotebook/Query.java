f2fa146eda5eb506e8aa6aba1d0c4b576124d596
hyunjun <hyunjun@ecologylab.net>
2010-08-04 04:12:43 +0000

    add @simpl_inherit

--- ecologylab/standalone/researchnotebook/Query.java
+++ ecologylab/standalone/researchnotebook/Query.java
@@ -3,6 +3,9 @@ package ecologylab.standalone.researchnotebook;
 import java.util.ArrayList;
 
 import ecologylab.serialization.ElementState;
+import ecologylab.serialization.simpl_inherit;
+
+@simpl_inherit
 
 public class Query extends ElementState{
 	@xml_tag("SearchTerms") @simpl_collection("SearchTerms") ArrayList<String> SearchTerms = new ArrayList<String>();  
