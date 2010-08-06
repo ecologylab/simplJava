05fd074b7b5a3836d957a4d96b589eecf2546cc2
hyunjun <hyunjun@ecologylab.net>
2010-08-06 14:25:27 +0000

    add getSearch()

--- ecologylab/standalone/researchnotebook/SeedSet.java
+++ ecologylab/standalone/researchnotebook/SeedSet.java
@@ -5,5 +5,9 @@ import java.util.ArrayList;
 import ecologylab.serialization.ElementState;
 
 public class SeedSet extends ElementState{
-	@simpl_nowrap @simpl_collection("search") ArrayList<Search> search = new ArrayList<Search>(); 
+	@simpl_nowrap @simpl_collection("search") ArrayList<Search> search = new ArrayList<Search>();
+	
+	public ArrayList<Search> getSearch(){
+		return search; 
+	}
 }
