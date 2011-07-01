cd543f44eaeddf038cc1f3e01c8544d1d3ed7e11
quyin <quyin@ecologylab.net>
2011-07-01 00:35:53 +0000

    New inheritance process for fields: MetaMetadataNestedField.inheritMetaMetadata(), MetaMetadataField.inheritAttributes(). The former inherits structures (kids) from inherited meta-metadata, sets attributes for first-level nested fields, and recursively inherit structures for nested fields. The latter is used by the former.
    The readability of the inheritance process is improved. Will finish and share the design document tonight.
    
    This new inheritance process can work alone or aid TranslationScope generation process, and is the basis for promoting inline mmd, polymorphism and ORM annotation generation.
    
    New compiler: passes test cases with the new inheritance process. Promoting inline mmd is nearly done. Will finish tonight.
    
    Other minor refactors to the field classes: moved some fields to the right place, added @mm_dont_inherit to comment, etc.

--- src/ecologylab/translators/metametadata/test/TestNewMetaMetadataCompiler.java
+++ src/ecologylab/translators/metametadata/test/TestNewMetaMetadataCompiler.java
@@ -14,34 +14,38 @@ import ecologylab.translators.java.JavaTranslator;
 public class TestNewMetaMetadataCompiler
 {
 	
-	public static void testGeneratingBasicTScope() throws IOException, SIMPLTranslationException, JavaTranslationException
+	private static void doTest(File testingRepository, File outputDir) throws IOException, SIMPLTranslationException, JavaTranslationException
 	{
-		MetaMetadataRepository repository = MetaMetadataRepository.readRepository(new File("data/testRepository/testGeneratingBasicTScope.xml"));
+		MetaMetadataRepository repository = MetaMetadataRepository.readRepository(testingRepository);
 		TranslationScope tscope = repository.traverseAndGenerateTranslationScope();
 		TranslationScope.setGraphSwitch();
 		JavaTranslator jt = new JavaTranslator();
 		TranslationScope metadataBuiltInTScope = MetadataBuiltinsTranslationScope.get();
 		for (ClassDescriptor cd : metadataBuiltInTScope.getClassDescriptors())
 			jt.excludeClassFromTranslation(cd);
-		jt.translateToJava(new File("c:/tmp/testbasictscope/"), tscope);
+		jt.translateToJava(outputDir, tscope);
+	}
+	
+	public static void testGeneratingBasicTScope() throws IOException, SIMPLTranslationException, JavaTranslationException
+	{
+		doTest(new File("data/testRepository/testGeneratingBasicTScope.xml"), new File("c:/tmp/testbasictscope/"));
 	}
 	
 	public static void testTypeGraphs() throws IOException, SIMPLTranslationException, JavaTranslationException
 	{
-		MetaMetadataRepository repository = MetaMetadataRepository.readRepository(new File("data/testRepository/testTypeGraphs.xml"));
-		TranslationScope tscope = repository.traverseAndGenerateTranslationScope();
-		TranslationScope.setGraphSwitch();
-		JavaTranslator jt = new JavaTranslator();
-		TranslationScope metadataBuiltInTScope = MetadataBuiltinsTranslationScope.get();
-		for (ClassDescriptor cd : metadataBuiltInTScope.getClassDescriptors())
-			jt.excludeClassFromTranslation(cd);
-		jt.translateToJava(new File("c:/tmp/testtypegraphs/"), tscope);
+		doTest(new File("data/testRepository/testTypeGraphs.xml"), new File("c:/tmp/testtypegraphs/"));
+	}
+
+	public static void testInlineMmd() throws IOException, SIMPLTranslationException, JavaTranslationException
+	{
+		doTest(new File("data/testRepository/testInlineMmd.xml"), new File("c:/tmp/testinlinemmd/"));
 	}
 
 	public static void main(String[] args) throws IOException, SIMPLTranslationException, JavaTranslationException
 	{
-//		testGeneratingBasicTScope();
+		testGeneratingBasicTScope();
 		testTypeGraphs();
+//		testInlineMmd();
 	}
 	
 }
