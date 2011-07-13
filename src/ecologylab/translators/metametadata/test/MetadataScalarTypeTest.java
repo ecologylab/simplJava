e043e3cca1c00d2bb09a6a58838078af88e8cc30
andruid <andruid@ecologylab.net>
2011-07-13 22:26:28 +0000

    new test case

---
+++ src/ecologylab/translators/metametadata/test/MetadataScalarTypeTest.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package ecologylab.translators.metametadata.test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.semantics.metametadata.MetaMetadataTranslationScope;
+import ecologylab.serialization.ElementState;
+import ecologylab.serialization.SIMPLTranslationException;
+import ecologylab.serialization.TranslationScope;
+
+/**
+ * @author andruid
+ *
+ */
+public class MetadataScalarTypeTest extends ElementState
+{
+	@simpl_collection
+	ArrayList<MetaMetadata>			metaMetadatas;
+	/**
+	 * 
+	 */
+	public MetadataScalarTypeTest()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] a)
+	{
+		MetaMetadataRepository.initializeTypes();
+		
+		TranslationScope mmTS	= MetaMetadataTranslationScope.get();
+		
+		File file	= new File("data/deserialization/simplMM.xml");
+		System.out.println(file.getAbsolutePath());
+		
+		try
+		{
+			ElementState	result = mmTS.deserialize(file);
+			
+			result.serialize(System.out);
+			System.out.println();
+		}
+		catch (SIMPLTranslationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
