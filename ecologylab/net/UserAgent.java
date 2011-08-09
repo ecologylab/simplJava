6c57dcfc536413702cf6d90798b5ffaaec874d9f
quyin <quyin@ecologylab.net>
2011-08-09 04:43:38 +0000

    Local MMD scope and TranslationScope implemented for meta-metadata.
    Handles inline meta-metadata / changing descriptors and meta-metadata with the same tag in a better way.
    
    Preliminary support for true packages.
    
    Refer to the new compiler design document for more details. (still ongoing)
    
    Tested with NewMmTest. Still need more testing (esp. with the dlese case).

--- ecologylab/net/UserAgent.java
+++ ecologylab/net/UserAgent.java
@@ -8,21 +8,35 @@ import ecologylab.serialization.types.element.Mappable;
 
 /**
  * @author awebb
- *
+ * 
  */
 public class UserAgent extends ElementState implements Mappable<String>
 {
-						@simpl_scalar 	String name;
-	@xml_tag("string")	@simpl_scalar 	String userAgentString;
-	@xml_tag("default") @simpl_scalar	boolean defaultAgent;
+	@simpl_scalar
+	String	name;
+
+	@xml_tag("string")
+	@simpl_scalar
+	String	userAgentString;
+
+	@xml_tag("default")
+	@simpl_scalar
+	boolean	defaultAgent;
+
+	public UserAgent()
+	{
+	}
 	
-	public UserAgent() { }
-	
+	public String name()
+	{
+		return name;
+	}
+
 	public String userAgentString()
 	{
 		return userAgentString;
 	}
-	
+
 	public boolean isDefaultAgent()
 	{
 		return defaultAgent;
