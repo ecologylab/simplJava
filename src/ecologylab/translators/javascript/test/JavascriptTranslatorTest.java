dbe3ae9a7eff211f75fe4f71a4514993b93f2980
rhema <rhema@ecologylab.net>
2011-08-09 22:41:07 +0000

    Graph serialization in javascript done.

--- src/ecologylab/translators/javascript/test/JavascriptTranslatorTest.java
+++ src/ecologylab/translators/javascript/test/JavascriptTranslatorTest.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import ecologylab.serialization.ElementState.FORMAT;
 
 import ecologylab.serialization.ClassDescriptor;
+import ecologylab.serialization.ElementState;
 import ecologylab.serialization.FieldDescriptor;
 import ecologylab.serialization.SIMPLTranslationException;
+import ecologylab.serialization.TranslationContext;
 import ecologylab.serialization.TranslationScope;
 import ecologylab.serialization.XMLTools;
 import ecologylab.translators.cocoa.CocoaTranslationException;
@@ -17,7 +19,8 @@ import ecologylab.translators.net.DotNetTranslationException;
 import ecologylab.translators.net.DotNetTranslator;
 
 public class JavascriptTranslatorTest {
-
+//"{"refer_to_self":{"some_data":"here is some stringness","refer_to_self":{"refer_to_self":{"simpl.ref":"65584869"}},"simpl.id":"65584869"}}"
+//"{"refer_to_self":{"some_data":"here is some stringness","refer_to_self":{"simpl.ref":"98475151"},"simpl.id":"98475151"}}"
 	public static void main(String args[]) throws IOException, SIMPLTranslationException, DotNetTranslationException, CocoaTranslationException
 	{
 		System.out.println("Javascript Translator");
@@ -44,8 +47,12 @@ public class JavascriptTranslatorTest {
 		System.out.println("var self1 = '"+self1.serialize(FORMAT.JSON).toString()+"';");
 		System.out.println("var self1 = '"+self2.serialize(FORMAT.JSON).toString()+"';");
 		
-		
-		
+		//String fromJs =   "{\"refer_to_self\":{\"some_data\":\"here is some stringness\",\"simpl.id\":\"77909673\",\"reference_to_self\":{\"simpl.ref\":\"77909673\"}}}";
+		//String fromSelf = "{\"refer_to_self\":{\"some_data\":\"I haver referenceToSelf\", \"simpl.id\":\"270071071\", \"reference_to_self\":{\"simpl.ref\":\"270071071\"}}}";
+		//String fffuu =    "{\"refer_to_self\":{\"some_data\":\"here is some stringness\",\"reference_to_self\":{\"refer_to_self\":{\"simpl.ref\":\"1351295\"}},\"simpl.id\":\"1351295\"}}";
+		//ElementState.
+	  // ElementState es = ts.deserializeByteArray(fromJs.getBytes(),FORMAT.JSON, new TranslationContext());
+		//  es.toString();
 
 		//not sure why this is failing
 		//coco.translateToObjC(new File("C:/testjs/coco"), ts);
