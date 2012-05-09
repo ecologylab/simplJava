028cc5047a87a034b5868bf3c502ee5d134eacf8
quyin <quyin@ecologylab.net>
2012-05-09 08:56:56 +0000

    previous generics support in SIMPL does not distinguish generic type variable definition and usage, which causes problems for fully supporting generics in meta-metadata. this commit improves generics support in SIMPL and lays the foundation for integrating with meta-metadata generics: - now definition and usage are handled separately. see the javadoc in GenericTypeVar for details. - generic type vars will be re-evaluted in subclass. FieldDescriptor may be cloned when necessary. - mmd code generation updated, but not yet done completely.
    any developers working with generics can take a look at GenericTypeVar's javadoc to see how it works in SIMPL.
    
    ecologylab.serialization: GenericTypeVar, ClassDescriptor, FieldDescriptor, XmlTools
    IFundamentalPlatformSpecifics, FundamentalPlatformSpecificsSun
    MetaMetadataCollectionField
    javaCodeTranslator.stg
    simplTestCases: tests.generics.*: Unit test for currently supported generics in SIMPL. should be used whenever new development is done on SIMPL generics!!
    
    p.s. about the representation of generics in java.lang.reflect:
    there are 4 interfaces representing type information with generics, all platform independent:
    - Type: the most general interface representing an abstract type. base class for the followings.
    - Class: concrete class.
    - TypeVariable: a generic type variable, e.g. T. can be assigned with another Type.
    - ParameterizedType: a type with generics, e.g. List<T>, Map<String, String>. has a list of parameters, which may or may not have arguments.
    - WildcardType: ? extends BaseType. can get the upper/lower bounds.

--- test/ecologylab/translators/java/generics/JavaTranslatorGenericTest.java
+++ test/ecologylab/translators/java/generics/JavaTranslatorGenericTest.java
@@ -2,6 +2,8 @@ package ecologylab.translators.java.generics;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,16 +13,45 @@ import ecologylab.serialization.FieldDescriptor;
 import ecologylab.serialization.GenericTypeVar;
 import ecologylab.serialization.SIMPLTranslationException;
 import ecologylab.serialization.SimplTypesScope;
+import ecologylab.serialization.annotations.simpl_scalar;
 import ecologylab.translators.CodeTranslatorConfig;
 import ecologylab.translators.java.JavaTranslationException;
 import ecologylab.translators.java.JavaTranslator;
 
 public class JavaTranslatorGenericTest
 {
-
-	@Test
-	public void testBasicGenerics() throws JavaTranslationException, IOException, SIMPLTranslationException
+	
+	public static class My<T extends Number>
 	{
+		@simpl_scalar
+		public T				v;
+
+		@simpl_scalar
+		public int			n;
+
+		@simpl_scalar
+		public Integer	o;
+
+		public List<My>	l;
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	public void testBasicGenerics() throws JavaTranslationException, IOException, SIMPLTranslationException, SecurityException, NoSuchFieldException
+	{
+		
+		SimplTypesScope s = SimplTypesScope.get("test", My.class);
+		Field f = My.class.getField("v");
+		Type t1 = f.getGenericType();
+		System.out.println(f.getGenericType());
+		f = My.class.getField("n");
+		Type t2 = f.getGenericType();
+		System.out.println(f.getGenericType());
+		ClassDescriptor cd = s.getClassDescriptorBySimpleName("My");
+		FieldDescriptor fd = cd.getFieldDescriptorByFieldName("v");
+		
+		
+		
 		SimplTypesScope scope = SimplTypesScope.get("test-basic-generics", SearchResult.class, Search.class, SocialSearchResult.class, SocialSearch.class);
 		
 		ClassDescriptor cdSearch = scope.getClassDescriptorBySimpleName("Search");
@@ -36,6 +67,21 @@ public class JavaTranslatorGenericTest
 		{
 			System.out.println(genericTypeVar.toString());
 		}
+		
+		cdSearch = scope.getClassDescriptorBySimpleName("SocialSearch");
+		classGenericTypeVars = cdSearch.getGenericTypeVars();
+		for (GenericTypeVar genericTypeVar : classGenericTypeVars)
+		{
+			System.out.println(genericTypeVar.toString());
+		}
+		
+		fdSearchResults = cdSearch.getFieldDescriptorByFieldName("searchResults");
+		fieldGenericTypeVars = fdSearchResults.getGenericTypeVars();
+		for (GenericTypeVar genericTypeVar : fieldGenericTypeVars)
+		{
+			System.out.println(genericTypeVar.toString());
+		}
+		
 		
 		CodeTranslatorConfig ctc = new CodeTranslatorConfig();
 		JavaTranslator jt = new JavaTranslator();
