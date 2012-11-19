79357354127ef9b2e6cf3d592adac87afd115bac
Tom White <tom.white@outlook.com>
2012-11-19 17:39:32 -0600

    moving standalone.researchnotebook out of fundamental

--- src/ecologylab/standalone/researchnotebook/testxml/TestSubItem2.java
+++
@@ -1,9 +0,0 @@
-package ecologylab.standalone.researchnotebook.testxml;
-
-import ecologylab.serialization.ElementState;
-import ecologylab.serialization.annotations.simpl_scalar;
-
-public class TestSubItem2 extends ElementState{
-	@simpl_scalar String priority; 
-
-}
