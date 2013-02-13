417ee9b5c05b09c113805dfd7f25519616c36d78
Tom White <tom.white@outlook.com>
2013-02-13 08:01:33 -0600

    cheekily moving the relevant code for science

--- simplCore/src/simpl/descriptions/indexers/ClassDescriptorIndexer.java
+++ simplCore/src/simpl/descriptions/indexers/ClassDescriptorIndexer.java
@@ -16,38 +16,6 @@ public class ClassDescriptorIndexer extends MultiIndexer<ClassDescriptor<?>> {
 
 	// Here are the different indexers we intend to index "by" 
 	
-	final class byTagName extends ItemIndexPredicate<ClassDescriptor<?>>
-	{
-		/**
-		 * Gets the identifier for this index; this allows us to make calls to .by("") for the indexer.
-		 * So indexer.by("tagname") lets us index by the ItemIndexPredicate identified as "tagname" 
-		 * (This one.)
-		 */
-		public String GetIndexIdentifier() {
-			return "tagname";
-		}
-
-		/**
-		 * ObtainIndex for this indexer doesn't make sense because we 
-		 * intend to index by BOTH the tag name and all "othertags"; thus
-		 * we override ObtainIndexes and return a list of index strings for the given item.
-		 */
-		public String ObtainIndex(ClassDescriptor<?> item) {
-			throw new RuntimeException("Obtainindex not supported for this indexer; use ObtainIndexes instead. \"byTagName\"");
-		}
-		
-		@Override
-		/**
-		 * We override ObtainIndexes in this case because we intend to return multiple indexes for a given object. 
-		 * In this case, we index by tag name and all "other tags" for a given class description (since they all 
-		 * refer to the same class descriptor, after all!)
-		 */
-		public Collection<String> ObtainIndexes(ClassDescriptor<?> item) {
-			Collection<String> ourIndexes = new LinkedList<String>();
-			return ourIndexes;
-		}
-	}
-	
 	final class byClassSimpleName extends ItemIndexPredicate<ClassDescriptor<?>>
 	{
 		public String GetIndexIdentifier() {
@@ -86,6 +54,38 @@ public class ClassDescriptorIndexer extends MultiIndexer<ClassDescriptor<?>> {
 		public String ObtainIndex(ClassDescriptor<?> item) {
 			// TODO Auto-generated method stub
 			return Integer.toString(item.getTagName().hashCode());
+		}
+	}
+	
+	final class byTagName extends ItemIndexPredicate<ClassDescriptor<?>>
+	{
+		/**
+		 * Gets the identifier for this index; this allows us to make calls to .by("") for the indexer.
+		 * So indexer.by("tagname") lets us index by the ItemIndexPredicate identified as "tagname" 
+		 * (This one.)
+		 */
+		public String GetIndexIdentifier() {
+			return "tagname";
+		}
+
+		/**
+		 * ObtainIndex for this indexer doesn't make sense because we 
+		 * intend to index by BOTH the tag name and all "othertags"; thus
+		 * we override ObtainIndexes and return a list of index strings for the given item.
+		 */
+		public String ObtainIndex(ClassDescriptor<?> item) {
+			throw new RuntimeException("Obtainindex not supported for this indexer; use ObtainIndexes instead. \"byTagName\"");
+		}
+		
+		@Override
+		/**
+		 * We override ObtainIndexes in this case because we intend to return multiple indexes for a given object. 
+		 * In this case, we index by tag name and all "other tags" for a given class description (since they all 
+		 * refer to the same class descriptor, after all!)
+		 */
+		public Collection<String> ObtainIndexes(ClassDescriptor<?> item) {
+			Collection<String> ourIndexes = new LinkedList<String>();
+			return ourIndexes;
 		}
 	}
 	
