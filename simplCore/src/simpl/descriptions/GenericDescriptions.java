dc26454231f7d56dd423b486373330e41b8d0edd
Tom White <tom.white@outlook.com>
2013-05-01 12:22:16 -0500

    Adding field descritpions

--- simplCore/src/simpl/descriptions/GenericDescriptions.java
+++ simplCore/src/simpl/descriptions/GenericDescriptions.java
@@ -1,5 +1,8 @@
 package simpl.descriptions;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +13,7 @@ import java.util.List;
 public class GenericDescriptions {
 	
 	/**
-	 * Obtains a list of type variables for a given class. 
+	 * Obtains a list of type variables for a given class.
 	 */
 	public static List<GenericTypeVar> getClassTypeVariables(Class<?> c)
 	{
@@ -25,6 +28,56 @@ public class GenericDescriptions {
 			}
 		}
 
+		return result;
+	}
+	
+	// TODO: This may need to be moved back to sun specifics.
+	// That looks a bit like a mess, and since I don' have an android environment
+	// I can't test this. :| 
+	
+	public static List<GenericTypeVar> getFieldTypeVariables(Field field)
+	{
+		// Create an array list for the result;
+		List<GenericTypeVar> result = new ArrayList<GenericTypeVar>();
+		
+		Type genericType = field.getGenericType();
+		
+		// Return an empty list for non-generic fields. 
+		if(genericType == null)
+		{
+			return result;
+		}
+		
+		// If we have a field, we need to obtain the generic context.
+		// Fetch the class descriptor to obtain this context
+		ClassDescriptor contextCD = ClassDescriptors.getClassDescriptor(field.getDeclaringClass());
+
+		// If it's a TypeVariable, it doesn't have further parameters. 
+		if (genericType instanceof TypeVariable)
+		{
+			TypeVariable tv = (TypeVariable) genericType;
+			GenericTypeVar g = GenericTypeVar.getGenericTypeVarRef(tv,contextCD.getGenericTypeVariables());//fieldDescriptor.getGenericTypeVarsContext());
+			result.add(g);
+		}
+		else if (genericType instanceof ParameterizedType)
+		{
+			// Cast to parametizedType to get access to the getActualTypeArguments() call
+			ParameterizedType parameterizedType = (ParameterizedType) genericType;
+			Type[] types = parameterizedType.getActualTypeArguments();
+
+			// If we have types, add them to the result
+			if(types != null)
+			{
+				for (Type t : types)
+				{
+					// Use the contextCD's generic type variables as context.
+					GenericTypeVar g = GenericTypeVar.getGenericTypeVarRef(t, contextCD.getGenericTypeVariables());
+					result.add(g);
+				}
+			}
+		}
+				
+		// Return the resulting list of GenericTypeVariables
 		return result;
 	}
 }
